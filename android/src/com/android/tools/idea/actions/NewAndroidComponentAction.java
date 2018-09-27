a40454ef6fd711bf403d2a34a61e06c33b5e191b
Bruno Buss <buss@google.com>
2018-09-27 11:33:26 +0100

    Add support to restrict templates to AndroidX-enabled projects.
    This allows us to mark templates that need AndroidX enabled, so we don't
    break users build when they try to add a template that depends on
    AndroidX in a project that has not migrated yet.
    
    This is how it appears when the project do not have AndroidX enabled:
    https://screenshot.googleplex.com/svFJovFoMrP
    
    After the user enabled AndroidX:
    https://screenshot.googleplex.com/7aoFp9HOpOE
    
    Test: existing + new + manually
    Bug: 116777901
    Change-Id: Ifd5841853d1aef211ab4b321882e44c1567c3ebb
    (cherry picked from commit 31268780cb4b5a54b32f9d00869441fd83dd8fb7)
    (cherry picked from commit d164b84636e670667cdda915a851db58adc2b2c8)

--- android/src/com/android/tools/idea/actions/NewAndroidComponentAction.java
+++ android/src/com/android/tools/idea/actions/NewAndroidComponentAction.java
@@ -33,14 +33,17 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import icons.AndroidIcons;
 import org.jetbrains.android.facet.AndroidFacet;
+import org.jetbrains.android.refactoring.MigrateToAndroidxUtil;
 import org.jetbrains.android.util.AndroidBundle;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.util.List;
 import java.util.Set;
+import org.jetbrains.annotations.Nullable;
 
 import static com.android.builder.model.AndroidProject.PROJECT_TYPE_INSTANTAPP;
+import static org.jetbrains.android.refactoring.MigrateToAndroidxUtil.isAndroidx;
 
 /**
  * An action to launch a wizard to create a component from a template.
@@ -55,6 +58,7 @@ public class NewAndroidComponentAction extends AnAction {
   private final String myTemplateName;
   private final int myMinSdkApi;
   private final int myMinBuildSdkApi;
+  private final boolean myAndroidXRequired;
   private boolean myShouldOpenFiles = true;
 
   public NewAndroidComponentAction(@NotNull String templateCategory, @NotNull String templateName, int minSdkVersion) {
@@ -62,12 +66,18 @@ public class NewAndroidComponentAction extends AnAction {
   }
 
   public NewAndroidComponentAction(@NotNull String templateCategory, @NotNull String templateName, int minSdkVersion, int minBuildSdkApi) {
+    this(templateCategory, templateName, minSdkVersion, minBuildSdkApi, false);
+  }
+
+  public NewAndroidComponentAction(@NotNull String templateCategory, @NotNull String templateName, int minSdkVersion, int minBuildSdkApi,
+                                   boolean androidXRequired) {
     super(templateName, AndroidBundle.message("android.wizard.action.new.component", templateName), null);
     myTemplateCategory = templateCategory;
     myTemplateName = templateName;
     getTemplatePresentation().setIcon(isActivityTemplate() ? AndroidIcons.Activity : AndroidIcons.AndroidFile);
     myMinSdkApi = minSdkVersion;
     myMinBuildSdkApi = minBuildSdkApi;
+    myAndroidXRequired = androidXRequired;
   }
 
   public void setShouldOpenFiles(boolean shouldOpenFiles) {
@@ -98,6 +108,10 @@ public class NewAndroidComponentAction extends AnAction {
     }
     else if (buildSdkVersion != null && myMinBuildSdkApi > buildSdkVersion.getFeatureLevel()) {
       presentation.setText(AndroidBundle.message("android.wizard.action.requires.minbuildsdk", myTemplateName, myMinBuildSdkApi));
+      presentation.setEnabled(false);
+    }
+    else if (myAndroidXRequired && !useAndroidX(module)) {
+      presentation.setText(AndroidBundle.message("android.wizard.action.requires.androidx", myTemplateName));
       presentation.setEnabled(false);
     }
     else {
@@ -162,5 +176,15 @@ public class NewAndroidComponentAction extends AnAction {
       view.selectElement(createdElement);
     }
     */
+  }
+
+  private static boolean useAndroidX(@Nullable Module module) {
+    if (module != null) {
+      Project project = module.getProject();
+      if (MigrateToAndroidxUtil.hasAndroidxProperty(module.getProject())) {
+        return isAndroidx(project);
+      }
+    }
+    return false;
   }
 }
