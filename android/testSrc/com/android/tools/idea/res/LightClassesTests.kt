a9c7cbdf5fb1f68d30cd7d333ebb4411c4e60313
Michal Bendowski <bendowski@google.com>
2019-01-16 16:23:17 +0000

    Teach ResourceFolderManager about generated resources
    These folders are already added to the project at sync time,
    so changes will be picked up by the existing listener mechanism.
    
    Bug: 120750247
    Test: LightClassesTest.kt
    Change-Id: I69220822b4bcdbb003bb86fa9b4b48603d591d20
    (cherry picked from commits e7e6243e3ee62dea92022fd61872f9212503df81
     and a220089b1a39c21b6aab61bccb595dd61927e199)

--- android/testSrc/com/android/tools/idea/res/LightClassesTests.kt
+++ android/testSrc/com/android/tools/idea/res/LightClassesTests.kt
@@ -17,17 +17,24 @@ package com.android.tools.idea.res
 
 import com.android.SdkConstants
 import com.android.builder.model.AndroidProject
+import com.android.ide.common.rendering.api.ResourceNamespace
+import com.android.resources.ResourceType
 import com.android.tools.idea.flags.StudioFlags
 import com.android.tools.idea.testing.AndroidGradleTestCase
 import com.android.tools.idea.testing.TestProjectPaths
 import com.android.tools.idea.testing.caret
 import com.android.tools.idea.testing.highlightedAs
+import com.android.tools.idea.testing.moveCaret
+import com.android.tools.idea.util.toVirtualFile
 import com.google.common.truth.Truth.assertThat
 import com.intellij.codeInsight.TargetElementUtil
 import com.intellij.lang.annotation.HighlightSeverity.ERROR
+import com.intellij.openapi.application.runWriteAction
 import com.intellij.openapi.command.WriteCommandAction.runWriteCommandAction
 import com.intellij.openapi.module.Module
+import com.intellij.openapi.project.guessProjectDir
 import com.intellij.openapi.vfs.LocalFileSystem
+import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiField
@@ -369,7 +376,7 @@ sealed class LightClassesTestBase : AndroidTestCase() {
       assertThat(myFixture.javaFacade.findClass("com.example.someLib.R", GlobalSearchScope.everythingScope(project)))
         .isNotNull()
     }
-    
+
     fun testResourceRename() {
       val strings = myFixture.addFileToProject(
         "/res/values/strings.xml",
@@ -1128,5 +1135,78 @@ class TestRClassesTest : AndroidGradleTestCase() {
 
     myFixture.configureFromExistingVirtualFile(normalClass)
     myFixture.checkHighlighting()
+  }
+}
+
+
+/**
+ * Tests for resources registered as generated with Gradle.
+ */
+class GeneratedResourcesTest : AndroidGradleTestCase() {
+
+  override fun setUp() {
+    super.setUp()
+    StudioFlags.IN_MEMORY_R_CLASSES.override(true)
+  }
+
+  override fun tearDown() {
+    try {
+      StudioFlags.IN_MEMORY_R_CLASSES.clearOverride()
+    }
+    finally {
+      super.tearDown()
+    }
+  }
+
+  /**
+   * Regression test for b/120750247.
+   */
+  fun testGeneratedRawResource() {
+    val projectRoot = prepareProjectForImport(TestProjectPaths.PROJECT_WITH_APPAND_LIB)
+
+    File(projectRoot, "app/build.gradle").appendText(
+      """
+      android {
+        String resGeneratePath = "${"$"}{buildDir}/generated/my_generated_resources/res"
+        def generateResTask = tasks.create(name: 'generateMyResources').doLast {
+            def rawDir = "${"$"}{resGeneratePath}/raw"
+            mkdir(rawDir)
+            file("${"$"}{rawDir}/sample_raw_resource").write("sample text")
+        }
+
+        def resDir = files(resGeneratePath).builtBy(generateResTask)
+
+        applicationVariants.all { variant ->
+            variant.registerGeneratedResFolders(resDir)
+        }
+      }
+      """.trimIndent())
+
+    requestSyncAndWait()
+
+    AndroidProjectRootListener.ensureSubscribed(project)
+    assertThat(ResourceRepositoryManager.getAppResources(myModules.appModule)!!
+                 .getResources(ResourceNamespace.RES_AUTO, ResourceType.RAW, "sample_raw_resource")).isEmpty()
+
+    // Work around b/120750247 by invoking the right task explicitly, instead of calling generateSources.
+    invokeGradleTasks(project, "generateMyResources")
+
+    runWriteAction {
+      VfsUtil.markDirtyAndRefresh(false, true, true, projectRoot.toVirtualFile(refresh = true))
+    }
+    UIUtil.dispatchAllInvocationEvents()
+
+    assertThat(ResourceRepositoryManager.getAppResources(myModules.appModule)!!
+                 .getResources(ResourceNamespace.RES_AUTO, ResourceType.RAW, "sample_raw_resource")).isNotEmpty()
+
+    myFixture.openFileInEditor(
+      project.guessProjectDir()!!
+        .findFileByRelativePath("app/src/main/java/com/example/projectwithappandlib/app/MainActivity.java")!!)
+
+    myFixture.moveCaret("int id = |item.getItemId();")
+    myFixture.type("R.raw.")
+    myFixture.completeBasic()
+
+    assertThat(myFixture.lookupElementStrings).containsExactly("sample_raw_resource", "class")
   }
 }
