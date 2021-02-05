JetBrains/android

Android: replace `ExecutorService.submit()` with `Executor.execute()` when execution result is not needed
This is mostly to make sure that exceptions are not swallowed and logged properly. E.g. see IDEA-248474: using `submit` instead of `execute` resulted in `AssertionError` (method should be executed from EDT) not to be logged (future result containing an exception has been ignored).

GitOrigin-RevId: 6e8f2038ac53b1218575b4a2f3fb574307d4b75b

profilers/testSrc/com/android/tools/profilers/memory/adapters/LiveAllocationCaptureObjectTest.java
@@ -42,11 +42,9 @@ import com.android.tools.profilers.memory.MemoryProfilerConfiguration;
 import com.android.tools.profilers.memory.MemoryProfilerStage;
 import com.android.tools.profilers.stacktrace.NativeFrameSymbolizer;
 import com.google.common.util.concurrent.MoreExecutors;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
-import java.util.concurrent.Callable;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
@@ -270,13 +268,12 @@ public class LiveAllocationCaptureObjectTest {
 
       // Adds a task that starts and blocks. This forces the subsequent selection change events to wait.
       CountDownLatch latch = new CountDownLatch(1);
-      capture.myExecutorService.submit((Callable<CaptureObject>)() -> {
+      capture.myExecutorService.execute(() -> {
         try {
           latch.await();
         }
         catch (Exception ignored) {
         }
-        return capture;
       });
 
       // Fake 4 selection range changes that would be cancelled.
@@ -883,14 +880,13 @@ public class LiveAllocationCaptureObjectTest {
   // NOTE - this works because myExecutorService is a single-threaded executor.
   private static void waitForLoadComplete(@NotNull LiveAllocationCaptureObject capture) throws InterruptedException, ExecutionException {
     CountDownLatch latch = new CountDownLatch(1);
-    capture.myExecutorService.invokeAny(Collections.singleton((Callable<CaptureObject>)() -> {
+    capture.myExecutorService.execute(() -> {
       try {
         latch.countDown();
       }
       catch (Exception ignored) {
       }
-      return capture;
-    }));
+    });
     latch.await();
   }
 

msft-mirror-aosp/platform.tools.adt.idea

(cherry picked from commit c1b23b0a997d0bf5cf515170e335075d3bbc8af3)
Change-Id: Ic1b23b0a997d0bf5cf515170e335075d3bbc8af3

profilers/testSrc/com/android/tools/profilers/memory/adapters/LiveAllocationCaptureObjectTest.java
@@ -43,11 +43,9 @@ import com.android.tools.profilers.memory.adapters.classifiers.HeapSet;
 import com.android.tools.profilers.memory.CaptureSelectionAspect;
 import com.android.tools.profilers.stacktrace.NativeFrameSymbolizer;
 import com.google.common.util.concurrent.MoreExecutors;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
-import java.util.concurrent.Callable;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
@@ -270,13 +268,12 @@ public class LiveAllocationCaptureObjectTest {
 
       // Adds a task that starts and blocks. This forces the subsequent selection change events to wait.
       CountDownLatch latch = new CountDownLatch(1);
-      capture.executorService.submit((Callable<CaptureObject>)() -> {
+      capture.executorService.execute(() -> {
         try {
           latch.await();
         }
         catch (Exception ignored) {
         }
-        return capture;
       });
 
       // Fake 4 selection range changes that would be cancelled.
@@ -866,14 +863,13 @@ public class LiveAllocationCaptureObjectTest {
   // NOTE - this works because myExecutorService is a single-threaded executor.
   private static void waitForLoadComplete(@NotNull LiveAllocationCaptureObject capture) throws InterruptedException, ExecutionException {
     CountDownLatch latch = new CountDownLatch(1);
-    capture.executorService.invokeAny(Collections.singleton((Callable<CaptureObject>)() -> {
+    capture.executorService.execute(() -> {
       try {
         latch.countDown();
       }
       catch (Exception ignored) {
       }
-      return capture;
-    }));
+    });
     latch.await();
   }
 