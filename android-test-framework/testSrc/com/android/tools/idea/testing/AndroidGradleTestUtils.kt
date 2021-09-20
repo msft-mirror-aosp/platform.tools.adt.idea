2d971a3124e08130b10fbe29d7368858eb26c63f
Yuki Hamada <hummer@google.com>
2021-09-20 14:27:06 -0700

    Don't inject useUnifiedTestPlatform Gradle property
    Instead of injecting the Gradle property
    android.experimental.androidTest.useUnifiedTestPlatform=true
    when "Run Android tests using Gradle" option is enabled,
    check the Gradle project's flag in the Gradle Builder
    model and falls back to the in-IDE test executor.
    
    Fixes: 200535156,200581253
    Test: tested manually
    Change-Id: I0adea1e5f6bf34a7dab392b177d7b4f6610a21c5

--- android-test-framework/testSrc/com/android/tools/idea/testing/AndroidGradleTestUtils.kt
+++ android-test-framework/testSrc/com/android/tools/idea/testing/AndroidGradleTestUtils.kt
@@ -481,7 +481,8 @@ fun AndroidProjectStubBuilder.buildAgpProjectFlagsStub(): IdeAndroidGradlePlugin
     testRClassConstantIds = true,
     transitiveRClasses = true,
     usesCompose = false,
-    mlModelBindingEnabled = mlModelBindingEnabled
+    mlModelBindingEnabled = mlModelBindingEnabled,
+    unifiedTestPlatformEnabled = true,
   )
 
 fun AndroidProjectStubBuilder.buildDefaultConfigStub() = IdeProductFlavorContainerImpl(
--- project-system-gradle-sync/src/com/android/tools/idea/gradle/project/sync/ModelCacheV2Impl.kt
+++ project-system-gradle-sync/src/com/android/tools/idea/gradle/project/sync/ModelCacheV2Impl.kt
@@ -1044,7 +1044,8 @@ internal fun modelCacheV2Impl(buildRootDirectory: File?): ModelCache {
         testRClassConstantIds = booleanFlagMap.getV2BooleanFlag(AndroidGradlePluginProjectFlags.BooleanFlag.TEST_R_CLASS_CONSTANT_IDS),
         transitiveRClasses = booleanFlagMap.getV2BooleanFlag(AndroidGradlePluginProjectFlags.BooleanFlag.TRANSITIVE_R_CLASS),
         usesCompose = booleanFlagMap.getV2BooleanFlag(AndroidGradlePluginProjectFlags.BooleanFlag.JETPACK_COMPOSE),
-        mlModelBindingEnabled = booleanFlagMap.getV2BooleanFlag(AndroidGradlePluginProjectFlags.BooleanFlag.ML_MODEL_BINDING)
+        mlModelBindingEnabled = booleanFlagMap.getV2BooleanFlag(AndroidGradlePluginProjectFlags.BooleanFlag.ML_MODEL_BINDING),
+        unifiedTestPlatformEnabled = booleanFlagMap.getV2BooleanFlag(AndroidGradlePluginProjectFlags.BooleanFlag.UNIFIED_TEST_PLATFORM),
       )
     }
     else {
@@ -1053,7 +1054,8 @@ internal fun modelCacheV2Impl(buildRootDirectory: File?): ModelCache {
         testRClassConstantIds = false,
         transitiveRClasses = false,
         usesCompose = false,
-        mlModelBindingEnabled = false
+        mlModelBindingEnabled = false,
+        unifiedTestPlatformEnabled = false,
       )
     }
   }
