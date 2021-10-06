f426c7416842d3ef66a36cc67be69197399d327f
Isaac Chai <ichai@google.com>
2021-10-06 15:02:00 -0700

    Remove old code path for ATF
    We no longer use "unoptimized" version of ATF integration path. Removing
    the code to clean up the code.
    
    This also update all tests (including perfgate test) to better
    reflect the current code path.
    
    With this cl I expect perfgate atf integration to go down drastically
    and we can use it as a better benchmark when new ATF library is added.
    
    Bug: 181892299
    Test: Manally tested
    Change-Id: I916e9083aeede5136414e1d0de7114c944acf5a5

--- android/src/com/android/tools/idea/rendering/RenderTask.java
+++ android/src/com/android/tools/idea/rendering/RenderTask.java
@@ -171,7 +171,6 @@ public class RenderTask {
   private boolean myShadowEnabled = true;
   private boolean myHighQualityShadow = true;
   private boolean myEnableLayoutScanner = false;
-  private boolean myEnableLayoutScannerOptimization = false;
   private boolean myShowWithToolsVisibilityAndPosition = true;
   private AssetRepositoryImpl myAssetRepository;
   private long myTimeout;
@@ -553,11 +552,6 @@ public class RenderTask {
     return this;
   }
 
-  public RenderTask setEnableLayoutScannerOptimization(boolean enableLayoutScannerOptimization) {
-    myEnableLayoutScannerOptimization = enableLayoutScannerOptimization;
-    return this;
-  }
-
   /**
    * Sets whether the rendering should use 'tools' namespaced 'visibility' and 'layout_editor_absoluteX/Y' attributes.
    * <p>
@@ -644,7 +638,8 @@ public class RenderTask {
     params.setFlag(RenderParamsFlags.FLAG_KEY_RENDER_HIGH_QUALITY_SHADOW, myHighQualityShadow);
     params.setFlag(RenderParamsFlags.FLAG_KEY_ENABLE_LAYOUT_SCANNER, myEnableLayoutScanner);
     params.setFlag(RenderParamsFlags.FLAG_ENABLE_LAYOUT_SCANNER_IMAGE_CHECK, myEnableLayoutScanner);
-    params.setFlag(RenderParamsFlags.FLAG_ENABLE_LAYOUT_SCANNER_OPTIMIZATION, myEnableLayoutScannerOptimization);
+    // TODO: Remove after ag/15997527
+    params.setFlag(RenderParamsFlags.FLAG_ENABLE_LAYOUT_SCANNER_OPTIMIZATION, myEnableLayoutScanner);
 
     // Request margin and baseline information.
     // TODO: Be smarter about setting this; start without it, and on the first request
