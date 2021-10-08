didot/android

[Gradle] update bundled Gradle TAPI IDEA-270533
GitOrigin-RevId: 32582f8f069b654f13758c83415c764cb13e0edd

android/testSrc/com/android/tools/idea/gradle/model/java/JavaModuleContentRootTest.java
@@ -49,37 +49,31 @@ public class JavaModuleContentRootTest {
     File rootDirPath = new File("root");
     expect(originalContentRoot.getRootDirectory()).andStubReturn(rootDirPath);
 
-    Pair<File, IdeaSourceDirectory> mock = createSourceDirectoryMock("src");
+    Pair<File, IdeaSourceDirectory> mock = createSourceDirectoryMock("src", false);
     File sourceDirPath = mock.getFirst();
     IdeaSourceDirectory sourceDir = mock.getSecond();
-    originalContentRoot.getSourceDirectories();
-    expectLastCall().andStubReturn(DomainObjectHashSet.newSet(sourceDir));
-
-    mock = createSourceDirectoryMock("gen-src");
+    mock = createSourceDirectoryMock("gen-src", true);
     File genSourceDirPath = mock.getFirst();
     IdeaSourceDirectory genSourceDir = mock.getSecond();
-    originalContentRoot.getGeneratedSourceDirectories();
-    expectLastCall().andStubReturn(DomainObjectHashSet.newSet(genSourceDir));
+    originalContentRoot.getSourceDirectories();
+    expectLastCall().andStubReturn(DomainObjectHashSet.newSet(sourceDir, genSourceDir));
 
-    mock = createSourceDirectoryMock("test");
+    mock = createSourceDirectoryMock("test", false);
     File testDirPath = mock.getFirst();
     IdeaSourceDirectory testDir = mock.getSecond();
-    originalContentRoot.getTestDirectories();
-    expectLastCall().andStubReturn(DomainObjectHashSet.newSet(testDir));
-
-    mock = createSourceDirectoryMock("gen-test");
+    mock = createSourceDirectoryMock("gen-test", true);
     File genTestDirPath = mock.getFirst();
     IdeaSourceDirectory genTestDir = mock.getSecond();
-    originalContentRoot.getGeneratedTestDirectories();
-    expectLastCall().andStubReturn(DomainObjectHashSet.newSet(genTestDir));
+    originalContentRoot.getTestDirectories();
+    expectLastCall().andStubReturn(DomainObjectHashSet.newSet(testDir, genTestDir));
 
-    mock = createSourceDirectoryMock("resource");
+    mock = createSourceDirectoryMock("resource", false);
     File resourceDirPath = mock.getFirst();
     IdeaSourceDirectory resourceDir = mock.getSecond();
     originalContentRoot.getResourceDirectories();
     expectLastCall().andStubReturn(DomainObjectHashSet.newSet(resourceDir));
 
-    mock = createSourceDirectoryMock("resource-test");
+    mock = createSourceDirectoryMock("resource-test", false);
     File testResourceDirPath = mock.getFirst();
     IdeaSourceDirectory testResourceDir = mock.getSecond();
     originalContentRoot.getTestResourceDirectories();
@@ -105,10 +99,11 @@ public class JavaModuleContentRootTest {
   }
 
   @NotNull
-  private static Pair<File, IdeaSourceDirectory> createSourceDirectoryMock(@NotNull String fileName) {
+  private static Pair<File, IdeaSourceDirectory> createSourceDirectoryMock(@NotNull String fileName, boolean isGenerated) {
     File path = new File(fileName);
     IdeaSourceDirectory dir = createMock(IdeaSourceDirectory.class);
     expect(dir.getDirectory()).andStubReturn(path);
+    expect(dir.isGenerated()).andStubReturn(isGenerated);
     return Pair.create(path, dir);
   }
 

msft-mirror-aosp/platform.tools.adt.idea

(cherry picked from commit 169073006dd1b3f9ec26301aea840dbe636706be)
Change-Id: I169073006dd1b3f9ec26301aea840dbe636706be

android/testSrc/com/android/tools/idea/gradle/model/java/JavaModuleContentRootTest.java
@@ -49,37 +49,31 @@ public class JavaModuleContentRootTest {
     File rootDirPath = new File("root");
     expect(originalContentRoot.getRootDirectory()).andStubReturn(rootDirPath);
 
-    Pair<File, IdeaSourceDirectory> mock = createSourceDirectoryMock("src");
+    Pair<File, IdeaSourceDirectory> mock = createSourceDirectoryMock("src", false);
     File sourceDirPath = mock.getFirst();
     IdeaSourceDirectory sourceDir = mock.getSecond();
-    originalContentRoot.getSourceDirectories();
-    expectLastCall().andStubReturn(DomainObjectHashSet.newSet(sourceDir));
-
-    mock = createSourceDirectoryMock("gen-src");
+    mock = createSourceDirectoryMock("gen-src", true);
     File genSourceDirPath = mock.getFirst();
     IdeaSourceDirectory genSourceDir = mock.getSecond();
-    originalContentRoot.getGeneratedSourceDirectories();
-    expectLastCall().andStubReturn(DomainObjectHashSet.newSet(genSourceDir));
+    originalContentRoot.getSourceDirectories();
+    expectLastCall().andStubReturn(DomainObjectHashSet.newSet(sourceDir, genSourceDir));
 
-    mock = createSourceDirectoryMock("test");
+    mock = createSourceDirectoryMock("test", false);
     File testDirPath = mock.getFirst();
     IdeaSourceDirectory testDir = mock.getSecond();
-    originalContentRoot.getTestDirectories();
-    expectLastCall().andStubReturn(DomainObjectHashSet.newSet(testDir));
-
-    mock = createSourceDirectoryMock("gen-test");
+    mock = createSourceDirectoryMock("gen-test", true);
     File genTestDirPath = mock.getFirst();
     IdeaSourceDirectory genTestDir = mock.getSecond();
-    originalContentRoot.getGeneratedTestDirectories();
-    expectLastCall().andStubReturn(DomainObjectHashSet.newSet(genTestDir));
+    originalContentRoot.getTestDirectories();
+    expectLastCall().andStubReturn(DomainObjectHashSet.newSet(testDir, genTestDir));
 
-    mock = createSourceDirectoryMock("resource");
+    mock = createSourceDirectoryMock("resource", false);
     File resourceDirPath = mock.getFirst();
     IdeaSourceDirectory resourceDir = mock.getSecond();
     originalContentRoot.getResourceDirectories();
     expectLastCall().andStubReturn(DomainObjectHashSet.newSet(resourceDir));
 
-    mock = createSourceDirectoryMock("test-resource");
+    mock = createSourceDirectoryMock("test-resource", false);
     File testResourceDirPath = mock.getFirst();
     IdeaSourceDirectory testResourceDir = mock.getSecond();
     originalContentRoot.getTestResourceDirectories();
@@ -105,10 +99,11 @@ public class JavaModuleContentRootTest {
   }
 
   @NotNull
-  private static Pair<File, IdeaSourceDirectory> createSourceDirectoryMock(@NotNull String fileName) {
+  private static Pair<File, IdeaSourceDirectory> createSourceDirectoryMock(@NotNull String fileName, boolean isGenerated) {
     File path = new File(fileName);
     IdeaSourceDirectory dir = createMock(IdeaSourceDirectory.class);
     expect(dir.getDirectory()).andStubReturn(path);
+    expect(dir.isGenerated()).andStubReturn(isGenerated);
     return Pair.create(path, dir);
   }
 