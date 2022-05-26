237f1adde802c83d5819200a3b9560db1e8e17d0
Polina Koval <kovalp@google.com>
2022-05-26 22:14:36 +0100

    Add necessary information for Apply Changes into Java Debugger process handler
    Fixes: 231647054
    Test: testAllInformationForApplyChangesAndPositionManger
    Change-Id: I132844d286b19a280927e62873fbb1c0271a2b35

--- android/src/com/android/tools/idea/run/tasks/ConnectJavaDebuggerTask.java
+++ android/src/com/android/tools/idea/run/tasks/ConnectJavaDebuggerTask.java
@@ -98,7 +98,8 @@ public class ConnectJavaDebuggerTask extends ConnectDebuggerTaskBase {
           return Unit.INSTANCE;
         }
       ).onSuccess(session -> {
-        processHandler.copyUserDataTo(session.getDebugProcess().getProcessHandler());
+        ProcessHandler debugProcessHandler = session.getDebugProcess().getProcessHandler();
+        captureLogcatOutput(client, debugProcessHandler);
         session.showSessionTab();
       });
       return null;
