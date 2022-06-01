didot/android

IDEA-284644 ExpUI. Toolbar: MacOS title color
GitOrigin-RevId: 0655c2e31a1c7db0dd216ae9b5cbcf0d90194053

adt-ui/src/test/java/com/android/tools/adtui/swing/HeadlessDialogTester.kt
@@ -353,7 +353,7 @@ private class HeadlessDialogWrapperPeer(
     UIUtil.decorateWindowHeader(rootPane)
     val window = window
     if (window is JDialog && !window.isUndecorated) {
-      UIUtil.setCustomTitleBar(window, rootPane) { runnable: Runnable ->
+      UIUtil.setTransparentTitleBar(window, rootPane) { runnable: Runnable ->
         Disposer.register(wrapper.disposable, runnable::run)
       }
     }

msft-mirror-aosp/platform.tools.adt.idea

(cherry picked from commit 60a6457658e026a2ac77e2133384f657c055109b)
Change-Id: I60a6457658e026a2ac77e2133384f657c055109b

adt-ui/src/test/java/com/android/tools/adtui/swing/HeadlessDialogTester.kt
@@ -370,7 +370,7 @@ private class HeadlessDialogWrapperPeer(
     UIUtil.decorateWindowHeader(rootPane)
     val window = window
     if (window is JDialog && !window.isUndecorated) {
-      ToolbarUtil.setCustomTitleBar(window, rootPane) { runnable: Runnable ->
+      ToolbarUtil.setTransparentTitleBar(window, rootPane) { runnable: Runnable ->
         Disposer.register(wrapper.disposable, runnable::run)
       }
     }