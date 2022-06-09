00ae8477b844948b12c55cf071782819a44541b5
Jerome Gaillard <jgaillard@google.com>
2022-06-09 20:11:31 +0100

    Load missing R ids from external libraries
    When non-transitive R classes are enabled, the resource ids from
    resources in external libraries are not added to the module R class.
    Hence we must look for the R classes from external dependencies and
    parse those as well as the project R classes.
    When parsing R classes from external dependencies, we must also account
    for whether the module we are considering is using final ids or not. If
    it is we must make sure that we do not create a light R class version in
    the LibraryResourceClassLoader, but instead we must let the class loader
    find the compiled R classes.
    
    Bug: 233862429
    Test: ModuleClassLoaderTest, LibraryResourceClassLoaderTest
    Change-Id: I7c536274e39a6028d0895abf61ff5d0a7bcabc5a
    (cherry picked from commit ff8bf5180c88492d20bb2bfaaa1976de9300bef1)

--- designer/testSrc/com/android/tools/idea/uibuilder/visual/visuallint/VisualLintAnalysisTest.kt
+++ designer/testSrc/com/android/tools/idea/uibuilder/visual/visuallint/VisualLintAnalysisTest.kt
@@ -23,6 +23,7 @@ import com.android.tools.idea.common.surface.DesignSurface
 import com.android.tools.idea.configurations.Configuration
 import com.android.tools.idea.rendering.RenderTask
 import com.android.tools.idea.rendering.RenderTestUtil
+import com.android.tools.idea.res.TestResourceIdManager
 import com.android.tools.idea.testing.AndroidGradleProjectRule
 import com.android.tools.idea.uibuilder.model.NlComponentRegistrar
 import com.intellij.lang.annotation.HighlightSeverity
@@ -46,6 +47,8 @@ class VisualLintAnalysisTest {
   @get:Rule
   val projectRule = AndroidGradleProjectRule()
 
+  private lateinit var resourceIdManger: TestResourceIdManager
+
   @Before
   fun setup() {
     projectRule.fixture.testDataPath = TestUtils.resolveWorkspacePath("tools/adt/idea/designer/testData").toString()
@@ -59,6 +62,7 @@ class VisualLintAnalysisTest {
     ApplicationManager.getApplication().invokeAndWait {
       RenderTestUtil.afterRenderTestCase()
     }
+    resourceIdManger.resetFinalIdsUsed()
   }
 
   @Test
@@ -67,6 +71,10 @@ class VisualLintAnalysisTest {
     projectRule.requestSyncAndWait()
 
     val module = projectRule.getModule("app")
+    // Disable final IDs for this test, so it can use light classes to resolve resources.
+    // Final IDs being enabled/disabled are covered by other tests, namely ModuleClassLoaderTest and LibraryResourceClassLoaderTest.
+    resourceIdManger = TestResourceIdManager.getManager(module)
+    resourceIdManger.setFinalIdsUsed(false)
     val facet = AndroidFacet.getInstance(module)!!
     val activityLayout = projectRule.project.baseDir.findFileByRelativePath("app/src/main/res/layout/activity_main.xml")!!
     val dashboardLayout = projectRule.project.baseDir.findFileByRelativePath("app/src/main/res/layout/fragment_dashboard.xml")!!
