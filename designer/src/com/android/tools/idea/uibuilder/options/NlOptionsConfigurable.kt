4ac7595d3bec94949269b90f213b8032494bcd23
Amaury Medeiros <amaurym@google.com>
2023-08-14 18:26:25 +0100

    Select Essentials Resource Usage when in Essentials Mode
    When Android Studio Essentials Mode is enabled, the Resource Usage radio
    button on the Editor > Design Tools settings page should also display as
    "Essentials", no matter which value the user had previously selected.
    
    Fixes: 295577903
    Test: N/A. Manually verified.
    Change-Id: I714ef0df434115df7769ab57887810409f49b138
    (cherry picked from commit 93a359cf44e912bbedc51fd0147ed1e84107bc52)

--- designer/src/com/android/tools/idea/uibuilder/options/NlOptionsConfigurable.kt
+++ designer/src/com/android/tools/idea/uibuilder/options/NlOptionsConfigurable.kt
@@ -161,7 +161,7 @@ class NlOptionsConfigurable : BoundConfigurable(DISPLAY_NAME), SearchableConfigu
             row {
                 defaultModeRadioButton =
                   radioButton("Default").bindSelected({
-                    !state.isComposePreviewEssentialsModeEnabled
+                    !state.isComposePreviewEssentialsModeEnabled && !EssentialsMode.isEnabled()
                   }) {
                     state.isComposePreviewEssentialsModeEnabled = !it
                   }
@@ -180,9 +180,9 @@ class NlOptionsConfigurable : BoundConfigurable(DISPLAY_NAME), SearchableConfigu
                   "Preview will preserve resources by inflating previews on demand, and disabling live updates and preview modes. " +
                     "<a href=\"https://developer.android.com/jetpack/compose/tooling/previews#essentials\">Learn more</a>"
 
-                radioButton("Essentials").comment(essentialsModeHint).bindSelected(
-                  state::isComposePreviewEssentialsModeEnabled
-                ) {
+                radioButton("Essentials").comment(essentialsModeHint).bindSelected({
+                  state.isComposePreviewEssentialsModeEnabled || EssentialsMode.isEnabled()
+                }) {
                   state.isComposePreviewEssentialsModeEnabled = it
                 }
               }
