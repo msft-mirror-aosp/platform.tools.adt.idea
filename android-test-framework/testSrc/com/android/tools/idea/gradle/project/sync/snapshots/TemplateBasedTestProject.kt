JetBrains/android

Android: Use TestUtils.resolveWorkspacePath to resolve test projects.
TestUtils.getWorkspaceRoot() is looking for test projects in wrong directory, TestUtils.resolveWorkspacePath should be used instead because it normalizes path from AS project structure to Idea project structure.

GitOrigin-RevId: d255672f2c9ce1c00da880ae8326bd30222abb47

android-test-framework/testSrc/com/android/tools/idea/gradle/project/sync/snapshots/TemplateBasedTestProject.kt
@@ -317,8 +317,7 @@ private fun <T : Any> updateXmlDoc(manifestPath: Path, transform: (Document) -> 
 }
 
 private fun TemplateBasedTestProject.resolveTestDataPath(testDataPath: @SystemIndependent String): File {
-  val testDataDirectory = TestUtils.getWorkspaceRoot()
-    .resolve(FileUtilRt.toSystemDependentName(getTestDataDirectoryWorkspaceRelativePath()))
+  val testDataDirectory = TestUtils.resolveWorkspacePath(FileUtilRt.toSystemDependentName(getTestDataDirectoryWorkspaceRelativePath()))
   return testDataDirectory.resolve(FileUtilRt.toSystemDependentName(testDataPath)).toFile()
 }
 

msft-mirror-aosp/platform.tools.adt.idea

(cherry-picked from cdd6982da7561ed59e349af4dfc6e7ad2731f16c)
Change-Id: Icdd6982da7561ed59e349af4dfc6e7ad2731f16c

android-test-framework/testSrc/com/android/tools/idea/gradle/project/sync/snapshots/TemplateBasedTestProject.kt
@@ -301,8 +301,7 @@ private fun <T : Any> updateXmlDoc(manifestPath: Path, transform: (Document) -> 
 }
 
 private fun TemplateBasedTestProject.resolveTestDataPath(testDataPath: @SystemIndependent String): File {
-  val testDataDirectory = TestUtils.getWorkspaceRoot()
-    .resolve(FileUtil.toSystemDependentName(getTestDataDirectoryWorkspaceRelativePath()))
+  val testDataDirectory = TestUtils.resolveWorkspacePath(FileUtil.toSystemDependentName(getTestDataDirectoryWorkspaceRelativePath()))
   return testDataDirectory.resolve(FileUtil.toSystemDependentName(testDataPath)).toFile()
 }
 