9b83fa7e89a5061da0dc7d47ffd4a8be9a8d236c
Matthew Gharrity <gharrma@google.com>
2024-03-28 00:04:47 +0000

    Update AI after Change I569c658d8
    Bug: 331651440
    Test: n/a
    Change-Id: I9bbe44661673f3be033d0bf5458e9dc92ce40ace

--- studio/integration/intellij.android.studio.integration/testSrc/com/android/tools/idea/StartUpTest.java
+++ studio/integration/intellij.android.studio.integration/testSrc/com/android/tools/idea/StartUpTest.java
@@ -104,7 +104,6 @@ public class StartUpTest {
         "Jetpack Compose",
         "Kotlin",
         "Machine Learning Code Completion",
-        "Machine Learning Code Completion Models",
         "Markdown",
         "Maven server api classes",
         "Mercurial",
