c1b8ec0d40c349b01899a22a38ddc32391fe2dca
Kainan Wang <knwang@google.com>
2024-05-10 16:35:36 -0700

    Enable monthly quota for device streaming by default
    Fixes: 339915492
    Test: none
    Change-Id: I8aab3647147dad17217c45e341c06f457ced7071
    (cherry picked from commit b6be1fde2086ef7ca016570cf153b76a1d35c404)

--- android-common/src/com/android/tools/idea/flags/StudioFlags.java
+++ android-common/src/com/android/tools/idea/flags/StudioFlags.java
@@ -1573,7 +1573,7 @@ public final class StudioFlags {
       "direct.access.monthly.quota",
       "Direct Access Monthly Quota",
       "Enable FTL DirectAccess Monthly Quota",
-      false);
+      true);
 
   public static final Flag<Boolean> DIRECT_ACCESS_CREATE_PROJECT =
     new BooleanFlag(
