f837809f9146ff4996cfb7ad3143ab297ab87443
Yuchen Yao <yaoyuchen@google.com>
2025-01-09 13:56:39 -0800

    Enable test scenario generation in stable
    Bug: 388890976
    Test: NA
    Change-Id: I3190a7ab9dc3ebb7a02d537c80c4a0f18760e440
    (cherry picked from commit efd8527f546d71e793fd03d36555f92cfe007a2b)

--- android-common/src/com/android/tools/idea/flags/StudioFlags.java
+++ android-common/src/com/android/tools/idea/flags/StudioFlags.java
@@ -2172,7 +2172,7 @@ public final class StudioFlags {
     new BooleanFlag(STUDIOBOT, "generate.test.scenarios",
                     "Enable test scenario generation.",
                     "When enabled, generate test scenarios and corresponding function names for the selected code.",
-                    enabledUpTo(CANARY));
+                    true);
 
   // endregion STUDIO_BOT
 
