JetBrains/android

[android] IDEA-362267 Move Gradle DSL Groovy implementation to intellij.android.dsl.groovy module
GitOrigin-RevId: 4215d4fcd22e77bfec4787fd83d6f956578a0dd8

gradle-dsl/src/com/android/tools/idea/gradle/dsl/model/ext/transforms/SdkOrPreviewTransform.java
@@ -25,13 +25,13 @@ import com.android.tools.idea.gradle.dsl.parser.elements.GradleDslExpression;
 import com.android.tools.idea.gradle.dsl.parser.elements.GradleDslSimpleExpression;
 import com.android.tools.idea.gradle.dsl.parser.elements.GradleNameElement;
 import com.android.tools.idea.gradle.dsl.parser.elements.GradlePropertiesDslElement;
-import com.android.tools.idea.gradle.dsl.parser.groovy.GroovyDslNameConverter;
 import com.android.tools.idea.gradle.dsl.parser.semantics.ModelEffectDescription;
 import com.android.tools.idea.gradle.dsl.parser.semantics.ModelPropertyDescription;
 import com.android.tools.idea.gradle.dsl.parser.semantics.ModelSemanticsDescription;
 import com.android.tools.idea.gradle.dsl.parser.semantics.VersionConstraint;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import java.util.Objects;
 
 /**
  * In AGP 7.0.0, some properties which previously had been set using a single operator with
@@ -101,15 +101,15 @@ public class SdkOrPreviewTransform extends PropertyTransform {
       }
       else if (value instanceof Integer) {
         operatorName = sdkSetter;
-        syntax = holder.getDslFile().getWriter() instanceof GroovyDslNameConverter
-                 ? ExternalNameInfo.ExternalNameSyntax.METHOD
-                 : ExternalNameInfo.ExternalNameSyntax.ASSIGNMENT;
+        syntax = Objects.equals("kts", holder.getDslFile().getFile().getExtension())
+                 ? ExternalNameInfo.ExternalNameSyntax.ASSIGNMENT
+                 : ExternalNameInfo.ExternalNameSyntax.METHOD;
       }
       else if (value instanceof String && ((String) value).startsWith("android-")) {
         operatorName = previewSetter;
-        syntax = holder.getDslFile().getWriter() instanceof GroovyDslNameConverter
-                 ? ExternalNameInfo.ExternalNameSyntax.METHOD
-                 : ExternalNameInfo.ExternalNameSyntax.ASSIGNMENT;
+        syntax = Objects.equals("kts", holder.getDslFile().getFile().getExtension())
+                 ? ExternalNameInfo.ExternalNameSyntax.ASSIGNMENT
+                 : ExternalNameInfo.ExternalNameSyntax.METHOD;
         value = ((String)value).substring("android-".length());
       }
       else { // RawText, literal Strings not beginning "android-"

msft-mirror-aosp/platform.tools.adt.idea

(cherry picked from commit 09b41ae762a4e592320988c75580f39fb50fe937)
Change-Id: I59923f14964becead626e274b1d155a7f939b900

gradle-dsl/src/com/android/tools/idea/gradle/dsl/model/ext/transforms/SdkOrPreviewTransform.java
@@ -26,13 +26,13 @@ import com.android.tools.idea.gradle.dsl.parser.elements.GradleDslExpression;
 import com.android.tools.idea.gradle.dsl.parser.elements.GradleDslSimpleExpression;
 import com.android.tools.idea.gradle.dsl.parser.elements.GradleNameElement;
 import com.android.tools.idea.gradle.dsl.parser.elements.GradlePropertiesDslElement;
-import com.android.tools.idea.gradle.dsl.parser.groovy.GroovyDslNameConverter;
 import com.android.tools.idea.gradle.dsl.parser.semantics.ModelEffectDescription;
 import com.android.tools.idea.gradle.dsl.parser.semantics.ModelPropertyDescription;
 import com.android.tools.idea.gradle.dsl.parser.semantics.ModelSemanticsDescription;
 import com.android.tools.idea.gradle.dsl.parser.semantics.VersionConstraint;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import java.util.Objects;
 
 /**
  * In AGP 7.0.0, some properties which previously had been set using a single operator with
@@ -102,13 +102,13 @@ public class SdkOrPreviewTransform extends PropertyTransform {
       }
       else if (value instanceof Integer) {
         operatorName = sdkSetter;
-        syntax = holder.getDslFile().getWriter() instanceof GroovyDslNameConverter
+        syntax = Objects.equals("gradle", holder.getDslFile().getFile().getExtension())
                  ? ExternalNameInfo.ExternalNameSyntax.METHOD
                  : ExternalNameInfo.ExternalNameSyntax.ASSIGNMENT;
       }
       else if (value instanceof String && ((String) value).startsWith("android-")) {
         operatorName = previewSetter;
-        syntax = holder.getDslFile().getWriter() instanceof GroovyDslNameConverter
+        syntax = Objects.equals("gradle", holder.getDslFile().getFile().getExtension())
                  ? ExternalNameInfo.ExternalNameSyntax.METHOD
                  : ExternalNameInfo.ExternalNameSyntax.ASSIGNMENT;
         value = ((String)value).substring("android-".length());