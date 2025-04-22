ec881276e536f3a21719b6deddf2b0db762c80b4
Alan Leung <acleung@google.com>
2025-04-22 11:35:58 -0700

    Give Deployment the Ability to Terminate
    The deployment process on newer Android versions (API 33+) can start to
    rely on the package manager to terminate an application before
    installation.
    
    However, in scenarios like install-without-install (IWI), the
    deployment logic may need to explicitly terminate the app.
    
    This change introduces a `DeployerApplicationTerminator` to provide the
    deployment flow with a formal mechanism for application termination.
    
    Bug: 412689373
    Test: Existing IWI ETE test should pick up the not killing issue (when we toggle the flag on)
    
    Change-Id: I603ba9a69b05359b715536c00c52b54b9f6b4ae4

--- android/src/com/android/tools/idea/testartifacts/instrumented/AndroidTestRunConfigurationExecutor.kt
+++ android/src/com/android/tools/idea/testartifacts/instrumented/AndroidTestRunConfigurationExecutor.kt
@@ -31,6 +31,7 @@ import com.android.tools.idea.run.ApkProvider
 import com.android.tools.idea.run.ClearLogcatListener
 import com.android.tools.idea.run.DeviceFutures
 import com.android.tools.idea.run.DeviceHeadsUpListener
+import com.android.tools.idea.run.ProcessHandlerApplicationTerminator
 import com.android.tools.idea.run.configuration.execution.createRunContentDescriptor
 import com.android.tools.idea.run.configuration.execution.getDevices
 import com.android.tools.idea.run.configuration.execution.println
@@ -151,7 +152,8 @@ constructor(
             project.messageBus.syncPublisher(ClearLogcatListener.TOPIC).clearLogcat(device.serialNumber)
           }
           LaunchUtils.initiateDismissKeyguard(device)
-          getDeployTask(device).run(device, indicator)
+          val terminator = ProcessHandlerApplicationTerminator(indicator, devices, packageName)
+          getDeployTask(device, terminator).run(device, indicator)
           // Notify listeners of the deployment.
           project.messageBus.syncPublisher(DeviceHeadsUpListener.TOPIC).launchingTest(device.serialNumber, project)
         }
@@ -164,7 +166,7 @@ constructor(
     Futures.whenAllComplete(futures).call({ processHandler.detachProcess() }, appExecutorService)
   }
 
-  private fun getDeployTask(device: IDevice): DeployTask {
+  private fun getDeployTask(device: IDevice, terminator: ProcessHandlerApplicationTerminator): DeployTask {
     val packages = apkProvider.getApks(device)
     val pmInstallOptions =
       if (device.version.apiLevel >= 23) {
@@ -174,7 +176,7 @@ constructor(
       }
     val containsMakeBeforeRun = configuration.beforeRunTasks.any { it.isEnabled }
 
-    return DeployTask(project, packages, pmInstallOptions, false, false, false, containsMakeBeforeRun)
+    return DeployTask(project, packages, terminator, pmInstallOptions, false, false, false, containsMakeBeforeRun)
   }
 
   /**
