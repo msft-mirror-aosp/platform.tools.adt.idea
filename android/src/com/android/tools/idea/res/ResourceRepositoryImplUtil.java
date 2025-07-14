52f1570ae69d83c8d793e3e925ad688fdf377bc0
Alexander.Glukhov <alexander.glukhov@jetbrains.com>
2025-07-14 15:24:45 +0200

    [gradle][classloader][IDEA-375876] do not use classes imported from `org.gradle.internal.impldep.*`
    (cherry picked from commit 60beea8ed9ae72a99a0045c6ae2b35f6f9723c5e)
    
    GitOrigin-RevId: a756de51e97980f702d743a4eccb93902a18d293
    Change-Id: If62f6ce0edf279d69ea94b693a57b9d63019dce4

--- android/src/com/android/tools/idea/res/ResourceRepositoryImplUtil.java
+++ android/src/com/android/tools/idea/res/ResourceRepositoryImplUtil.java
@@ -23,7 +23,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 /**
- * Helper methods for classes implementing {@link org.gradle.internal.impldep.aQute.bnd.service.repository.ResourceRepository} interface.
+ * Helper methods for classes implementing {@link com.android.ide.common.resources.ResourceRepository} interface.
  */
 class ResourceRepositoryImplUtil {
   /**
