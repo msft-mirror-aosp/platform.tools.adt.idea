2833cf4089b1786174ccc08be0ec5d58f2140762
David Belcher <davidbelcher@google.com>
2025-08-18 17:53:55 +0100

    Configure custom run configuration for test suites
    A new `TestSuiteRunConfiguration` has been added which extends
    `GradleRunConfiguration`. It can be configured with the following
    options:
    - deployable to device
    - show results in either the default IntelliJ test panel or the Android
      test matrix
    
    A `TestSuiteRunConfigurationProducer` has also been registered to create
    run configurations for generic test suites.
    
    Test: Added
    Bug: 433522989
    Change-Id: I8f3070bf00ef9144d5b76a7eb40477b162f0ed37

--- android-test-framework/testSrc/com/android/tools/idea/testing/AndroidGradleTestUtils.kt
+++ android-test-framework/testSrc/com/android/tools/idea/testing/AndroidGradleTestUtils.kt
@@ -70,6 +70,7 @@ import com.android.tools.idea.gradle.model.impl.IdeProductFlavorContainerImpl
 import com.android.tools.idea.gradle.model.impl.IdeProductFlavorImpl
 import com.android.tools.idea.gradle.model.impl.IdeProjectPathImpl
 import com.android.tools.idea.gradle.model.impl.IdeSourceProviderContainerImpl
+import com.android.tools.idea.gradle.model.impl.IdeTestSuiteImpl
 import com.android.tools.idea.gradle.model.impl.IdeTestSuiteTargetImpl
 import com.android.tools.idea.gradle.model.impl.IdeTestSuiteVariantTargetImpl
 import com.android.tools.idea.gradle.model.impl.IdeVariantBuildInformationImpl
@@ -452,6 +453,7 @@ interface AndroidProjectStubBuilder {
   val internedModels: InternedModels
   val defaultVariantName: String?
   val includeShadersSources: Boolean
+  val testSuites: List<IdeTestSuiteImpl>
 }
 
 /**
@@ -524,6 +526,7 @@ data class AndroidProjectBuilder(
   val includeBuildConfigSources: AndroidProjectStubBuilder.() -> Boolean = { false },
   val defaultVariantName: AndroidProjectStubBuilder.() -> String? = { null },
   val includeShadersSources: AndroidProjectStubBuilder.() -> Boolean = { false },
+  val testSuites: AndroidProjectStubBuilder.() -> List<IdeTestSuiteImpl> = { emptyList() }
 ) {
   fun withBuildId(buildId: AndroidProjectStubBuilder.() -> String) =
     copy(buildId = buildId)
@@ -621,7 +624,6 @@ data class AndroidProjectBuilder(
 
   fun withNamespace(namespace: String) = copy(namespace = {namespace})
 
-
   fun build(): AndroidProjectBuilderCore =
     fun(
       projectName: String,
@@ -692,6 +694,7 @@ data class AndroidProjectBuilder(
         override val internedModels: InternedModels get() = internedModels
         override val defaultVariantName: String? get() = defaultVariantName()
         override val includeShadersSources: Boolean get() = includeShadersSources()
+        override val testSuites: List<IdeTestSuiteImpl> get() = testSuites()
       }
       return AndroidProjectModels(
         androidProject = builder.androidProject,
@@ -1381,7 +1384,7 @@ fun AndroidProjectStubBuilder.buildAndroidProjectStub(): IdeAndroidProjectImpl {
     desugarLibraryConfigFiles = listOf(),
     defaultVariantName = defaultVariantName,
     lintJar = null,
-    testSuites = emptyList()
+    testSuites = testSuites
   )
 }
 
