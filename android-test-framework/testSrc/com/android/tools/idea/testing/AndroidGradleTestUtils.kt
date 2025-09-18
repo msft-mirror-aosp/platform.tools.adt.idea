a5db584ea46e4e00cdd6204f51f19f0efeb53e3d
Christophe Rhodes <xof@google.com>
2025-09-18 12:09:59 +0000

    More restrictive @RunsInEdt for withIntegrationTestEnvironment
    Running the whole of a test method, including project open, in the
    Application's Event Dispatch Thread is both more than most tests
    require, and likely to lead to problems on further changes to the
    underlying threading model beyond the deadlocks that we have already
    seen.  The common case for use of @RunsInEdt is so that some of the
    actual test body can run without requiring insertion of explicit
    `runInEdtAndWait {...}` or similar thread handling.
    
    For withIntegrationTestEnvironment, then, we will achieve the desired
    behaviour by affecting just the behaviour of
    
      openPreparedProject { p -> ... }
    
    and specifically which thread the action { p -> ... } should be run
    on, based only on whether the test is annotated with @RunsInEdt or
    not.  (We want this because there are hundreds of tests written with
    @RunsInEdt, while the EdtRule interpretation of @RunsInEdt is
    incompatible with the way we prepare and open the project.)
    
    In a language which supported dynamic binding, this would look
    something like
    
      (defmacro define-maybe-edt-test ((name &key edt) &body body)
        `(define-test name
           (let ((*run-action-on-edt* ',edt))
             ,@body)))
    
    along with
    
      (apply (if *run-action-on-edt* #'funcall-on-edt #'funcall) args)
    
    at the point where the action is called.
    
    We don't have a binding stack.  However, what we do have is an
    IntegrationTestEnvironment, which is propagated essentially all
    the way from the rule instantiation to the project open.  And, since
    it's an Environment I don't feel too bad about stuffing something that
    looks like a binding in there, for lookup at project open time.
    
    So, extend IntegrationTestEnvironment with this property.  Arrange for
    withIntegrationTestEnvironment() to populate it based on @RunsInEdt
    annotations, and for project openers to respect the value.  And hey
    presto, Bob is probably your uncle.
    
    Adjust the relatively few tests that need adjusting for this new
    interpretation of @RunsInEdt.
    
    Bug: 430533136
    Test: existing
    Change-Id: Ie9cad48822055f8d06b45301e5486ff032f3b5fb

--- android-test-framework/testSrc/com/android/tools/idea/testing/AndroidGradleTestUtils.kt
+++ android-test-framework/testSrc/com/android/tools/idea/testing/AndroidGradleTestUtils.kt
@@ -217,8 +217,10 @@ import com.intellij.testFramework.PlatformTestUtil
 import com.intellij.testFramework.fixtures.JavaCodeInsightTestFixture
 import com.intellij.testFramework.fixtures.impl.CodeInsightTestFixtureImpl.ensureIndexesUpToDate
 import com.intellij.testFramework.replaceService
+import com.intellij.testFramework.runInEdtAndGet
 import com.intellij.testFramework.runInEdtAndWait
 import com.intellij.util.ThrowableConsumer
+import com.intellij.util.concurrency.annotations.RequiresBackgroundThread
 import com.intellij.util.containers.ContainerUtil
 import com.intellij.util.containers.MultiMap
 import com.intellij.util.messages.MessageBusConnection
@@ -2273,6 +2275,11 @@ interface IntegrationTestEnvironment {
    * The base test directory to be used in tests.
    */
   fun getBaseTestPath(): @SystemDependent String
+
+  /**
+   * Whether to run the body of a prepared project open on the EDT.
+   */
+  val runOpenBodyOnEdt get() = false
 }
 
 /**
@@ -2391,10 +2398,12 @@ fun <T> IntegrationTestEnvironment.openPreparedProject(
   options: OpenPreparedProjectOptions = OpenPreparedProjectOptions(),
   action: (Project) -> T
 ): T {
-  return openPreparedProject(nameToPath(name), options, action)
+  return openPreparedProject(this, nameToPath(name), options, action)
 }
 
+@RequiresBackgroundThread
 private fun <T> openPreparedProject(
+  integrationTestEnvironment: IntegrationTestEnvironment,
   projectPath: File,
   options: OpenPreparedProjectOptions,
   action: (Project) -> T
@@ -2513,7 +2522,10 @@ private fun <T> openPreparedProject(
       try {
         verifyNoSyncIssues(project, options.expectedSyncIssues)
         options.verifyOpened(project)
-        return action(project)
+        return when (integrationTestEnvironment.runOpenBodyOnEdt) {
+          true -> runInEdtAndGet { action(project) }
+          false -> action(project)
+        }
       }
       finally {
         runInEdtAndWait {
