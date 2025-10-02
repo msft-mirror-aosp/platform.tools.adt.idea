0680e3260fc2408155cbea9eb53eaf498f8975b6
David Belcher <davidbelcher@google.com>
2025-10-02 12:00:37 +0100

    Fix unnecessary copying of collections when loading source set modules
    Bug: 433523616
    Test: Existing
    Change-Id: Ie47fa0f85d29a97f139e10505c8edd1021e54882

--- project-system-gradle/src/com/android/tools/idea/gradle/project/sync/idea/AndroidSourceRootSyncContributor.kt
+++ project-system-gradle/src/com/android/tools/idea/gradle/project/sync/idea/AndroidSourceRootSyncContributor.kt
@@ -372,13 +372,13 @@ class AndroidSourceRootSyncContributor : GradleSyncContributor {
         val sourceSetModuleEntitiesByArtifact = getAllSourceSetModuleEntities()
 
         val knownArtifactsModuleEntitiesByArtifact = sourceSetModuleEntitiesByArtifact.knownArtifacts
-        val knownArtifactsModuleEntities = knownArtifactsModuleEntitiesByArtifact.values.toList()
+        val knownArtifactsModuleEntities = knownArtifactsModuleEntitiesByArtifact.values
         if (knownArtifactsModuleEntities.isEmpty()) {
           LOG.debug("No source sets found for ${projectModel.path}")
           return@flatMap emptyList()
         }
 
-        val testSuiteSourceSetModules = sourceSetModuleEntitiesByArtifact.testSuites.values.toList()
+        val testSuiteSourceSetModules = sourceSetModuleEntitiesByArtifact.testSuites.values
 
         updatedEntities.modifyModuleEntity(holderModuleEntity) {
           setJavaSettingsForHolderModule(this)
@@ -479,7 +479,7 @@ private fun SyncContributorAndroidProjectContext.getAllSourceSetModuleEntities()
     newModuleEntity.contentRoots += createContentRootEntities(moduleName, entitySource, typeToDirsMap)
     newModuleEntity.javaSettings = createJavaModuleSettingsEntity(entitySource, sourceSetArtifactName)
     sourceSetArtifactName to newModuleEntity
-  }.toMutableMap()
+  }
 
   val testSuitesEnabled = StudioFlags.AGP_TEST_SUITES_ENABLED.get() && versions[ModelFeature.HAS_TEST_SUITES]
   val testSuiteSources = if (testSuitesEnabled) {
@@ -538,7 +538,7 @@ private fun SyncContributorAndroidProjectContext.configureTestSuiteSourceSetModu
 private fun SyncContributorAndroidProjectContext.linkModuleGroup(
   holderModuleEntity: ModuleEntity.Builder,
   sourceSetModules: Map<IdeArtifactName, ModuleEntity.Builder>,
-  testSuiteModules: List<ModuleEntity.Builder>
+  testSuiteModules: Collection<ModuleEntity.Builder>
 ) {
   val androidModuleGroup = getModuleGroup(sourceSetModules, testSuiteModules)
   val linkedModules = sourceSetModules.values + testSuiteModules + holderModuleEntity
@@ -564,7 +564,7 @@ private fun SyncContributorAndroidProjectContext.linkModuleGroup(
 
 private fun SyncContributorAndroidProjectContext.getModuleGroup(
   sourceSetModules: Map<IdeArtifactName, ModuleEntity.Builder>,
-  testSuiteModules: List<ModuleEntity.Builder>
+  testSuiteModules: Collection<ModuleEntity.Builder>
 ): LinkedAndroidGradleModuleGroup {
   val modulePointerManager = ModulePointerManager.getInstance(project)
   return LinkedAndroidGradleModuleGroup(
