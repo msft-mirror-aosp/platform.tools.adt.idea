3b6c2c7458a11ae53ee1f9daac6c210c054a0e10
Luis Santiago Re <lsantire@google.com>
2025-10-16 16:35:13 +0000

    Stop using deprecated workerThread in preview-designer
    workerThread is being replaced with Dispatchers.Default, following
    https://plugins.jetbrains.com/docs/intellij/coroutine-dispatchers.html
    
    Bug: 421128159
    Test: existing should pass
    Change-Id: Iacc7491b29ee37b75723ff84e9076bf9cb053e0d

--- preview-designer/testSrc/com/android/tools/idea/preview/views/CommonNlDesignSurfacePreviewViewTest.kt
+++ preview-designer/testSrc/com/android/tools/idea/preview/views/CommonNlDesignSurfacePreviewViewTest.kt
@@ -22,7 +22,6 @@ import com.android.tools.adtui.instructions.TextInstruction
 import com.android.tools.adtui.stdui.UrlData
 import com.android.tools.adtui.swing.FakeUi
 import com.android.tools.idea.concurrency.AndroidDispatchers.uiThread
-import com.android.tools.idea.concurrency.AndroidDispatchers.workerThread
 import com.android.tools.idea.testing.AndroidProjectRule
 import com.android.tools.idea.uibuilder.surface.NlSurfaceBuilder
 import com.intellij.openapi.project.Project
@@ -33,6 +32,7 @@ import java.awt.Dimension
 import javax.swing.JComponent
 import javax.swing.JLabel
 import javax.swing.JPanel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
@@ -93,7 +93,7 @@ class CommonNlDesignSurfacePreviewViewTest {
 
   @Test
   fun testShowLoading() =
-    runBlocking(workerThread) {
+    runBlocking(Dispatchers.Default) {
       withContext(uiThread) {
         previewView.showLoadingMessage("Loading foo")
         fakeUi.root.validate()
@@ -117,7 +117,7 @@ class CommonNlDesignSurfacePreviewViewTest {
 
   @Test
   fun testErrorMessage() =
-    runBlocking(workerThread) {
+    runBlocking(Dispatchers.Default) {
       withContext(uiThread) {
         previewView.showErrorMessage(
           "error foo happened",
