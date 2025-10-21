a7472763baf329804a6daf195ff8f132adbfdd39
Alexander Demurov <ademurov@google.com>
2025-10-21 16:58:16 +0000

    Remove the use of `FakeAdbServiceRule`
    Tests that used to rely on this rule no longer need it.
    
    Bug: n/a
    Test: existing
    Change-Id: I4f0f58b5ae25e5e41542b1e38bf732ba23ef941d

--- android-adb/testSrc/com/android/tools/idea/adblib/AdbServerStatusReporterTest.kt
+++ android-adb/testSrc/com/android/tools/idea/adblib/AdbServerStatusReporterTest.kt
@@ -18,7 +18,6 @@ package com.android.tools.idea.adblib
 
 import com.android.adblib.ServerStatus
 import com.android.ddmlib.testing.FakeAdbRule
-import com.android.tools.idea.adb.FakeAdbServiceRule
 import com.android.tools.idea.adb.InitAdbLibApplicationServiceRule
 import com.intellij.testFramework.ProjectRule
 import java.util.concurrent.CountDownLatch
@@ -34,7 +33,6 @@ class AdbServerStatusReporterTest {
   private val projectRule = ProjectRule()
   private val initAdbLibApplicationServiceRule = InitAdbLibApplicationServiceRule()
   private val adbRule = FakeAdbRule()
-  private val adbServiceRule = FakeAdbServiceRule(projectRule::project, adbRule)
   private lateinit var reporter: AdbServerStatusReporter
 
   private var statusCallbackCalled = false
@@ -42,10 +40,7 @@ class AdbServerStatusReporterTest {
 
   @get:Rule
   val ruleChain =
-    RuleChain.outerRule(projectRule)
-      .around(initAdbLibApplicationServiceRule)
-      .around(adbRule)
-      .around(adbServiceRule)!!
+    RuleChain.outerRule(projectRule).around(initAdbLibApplicationServiceRule).around(adbRule)!!
 
   private fun statusReporterCallback(status: ServerStatus) {
     Assert.assertNotNull("No server-status version", status.version)
