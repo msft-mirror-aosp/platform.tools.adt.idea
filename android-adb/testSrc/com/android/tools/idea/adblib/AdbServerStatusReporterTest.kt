b23fce016ce256f2ae1dc809974709e87a7fe71b
Alexander Demurov <ademurov@google.com>
2025-10-22 00:25:58 +0000

    Stop using `FakeAdbRule` in `android-adb` tests
    Stops the usage of `com.android.ddmlib.testing.FakeAdbRule`
    in `android-adb` tests.
    
    Bug: 443319528
    Test: these are tests
    Change-Id: I8498c3e21a75b033580aac29b73696f4db630621

--- android-adb/testSrc/com/android/tools/idea/adblib/AdbServerStatusReporterTest.kt
+++ android-adb/testSrc/com/android/tools/idea/adblib/AdbServerStatusReporterTest.kt
@@ -17,7 +17,8 @@
 package com.android.tools.idea.adblib
 
 import com.android.adblib.ServerStatus
-import com.android.ddmlib.testing.FakeAdbRule
+import com.android.adblib.ddmlibcompatibility.testutils.InitAndroidDebugBridgeRule
+import com.android.adblib.testingutils.FakeAdbServerRule
 import com.android.tools.idea.adb.InitAdbLibApplicationServiceRule
 import com.intellij.testFramework.ProjectRule
 import java.util.concurrent.CountDownLatch
@@ -32,7 +33,9 @@ import org.junit.rules.RuleChain
 class AdbServerStatusReporterTest {
   private val projectRule = ProjectRule()
   private val initAdbLibApplicationServiceRule = InitAdbLibApplicationServiceRule()
-  private val adbRule = FakeAdbRule()
+  private val fakeAdbRule = FakeAdbServerRule()
+  private val initAndroidDebugBridgeRule =
+    InitAndroidDebugBridgeRule(alsoCreateBridge = true) { fakeAdbRule.adbServer.port }
   private lateinit var reporter: AdbServerStatusReporter
 
   private var statusCallbackCalled = false
@@ -40,7 +43,10 @@ class AdbServerStatusReporterTest {
 
   @get:Rule
   val ruleChain =
-    RuleChain.outerRule(projectRule).around(initAdbLibApplicationServiceRule).around(adbRule)!!
+    RuleChain.outerRule(projectRule)
+      .around(initAdbLibApplicationServiceRule)
+      .around(fakeAdbRule)
+      .around(initAndroidDebugBridgeRule)!!
 
   private fun statusReporterCallback(status: ServerStatus) {
     Assert.assertNotNull("No server-status version", status.version)
