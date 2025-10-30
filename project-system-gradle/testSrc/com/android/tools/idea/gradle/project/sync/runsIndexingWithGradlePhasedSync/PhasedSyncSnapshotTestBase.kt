e9757a1093b8031d0e6589c6f89376dfaf9524ff
Jerome Dochez <jedo@google.com>
2025-10-29 17:11:15 -0700

    remove privacy sandbox related tests
    Test: Existing
    Bug: N/A
    Change-Id: Ic2505b8ccea8a72bf07195e11b2a9d3de64f48c8

--- project-system-gradle/testSrc/com/android/tools/idea/gradle/project/sync/runsIndexingWithGradlePhasedSync/PhasedSyncSnapshotTestBase.kt
+++ project-system-gradle/testSrc/com/android/tools/idea/gradle/project/sync/runsIndexingWithGradlePhasedSync/PhasedSyncSnapshotTestBase.kt
@@ -91,7 +91,6 @@ abstract class PhasedSyncSnapshotTestBase {
       TestProject.LIGHT_SYNC_REFERENCE,
       TestProject.NON_STANDARD_SOURCE_SETS,
       TestProject.BUILDSRC_WITH_COMPOSITE,
-      TestProject.PRIVACY_SANDBOX_SDK,
       TestProject.APP_WITH_BUILD_FEATURES_ENABLED,
       TestProject.DEPENDENT_MODULES_ONLY_APP_RUNTIME,
       TestProject.BUILD_CONFIG_AS_BYTECODE_ENABLED,
