a49e44c7bd9079bcda2ac5e56dd2a5b96711c2d4
Karima Ichou <karimai@google.com>
2025-11-05 17:04:48 +0000

    Add missingDimensionStrategy to the IDE models
    Bug: N/A
    Test: updated
    Change-Id: Icaea2a87434724e0dfb6c2c0f952deb7e87e8e50

--- android-test-framework/testSrc/com/android/tools/idea/testing/AndroidGradleTestUtils.kt
+++ android-test-framework/testSrc/com/android/tools/idea/testing/AndroidGradleTestUtils.kt
@@ -1086,6 +1086,7 @@ fun AndroidProjectStubBuilder.buildDefaultConfigStub() =
         resValues = emptyMap(),
         versionNameSuffix = null,
         matchingFallbacks = emptyList(),
+        missingDimensionStrategy = emptyMap(),
         isDefault = null,
       ),
     sourceProvider = mainSourceProvider,
