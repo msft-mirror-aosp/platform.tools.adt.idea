d2872b75b15dce0871160883e3fa410cb54a66ed
Maksim Lazeba <mlazeba@google.com>
2025-11-12 17:22:20 +0000

    Do not dispatch output events when not necessary
    When gradle execution is requested via AndroidGradleTaskManager from
    internals of the IJ platform (which is the case for running unit tests)
    it comes with the listener that internally notifies events dispatcher.
    As we are manually notifying the dispatcher here, it gets notified
    twice for each event which results in duplicated lines.
    
    startBuildEventPosted is a good indication of if this is needed or not,
    build output infrastructure being properly set up before us here also
    means dispatcher is being handled correctly already.
    
    Fixes: 301232493
    Test: Existing integration tests should catch any regressions, manually
    verified that duplicated output issue is fixed with this.
    Change-Id: I1941261a49ab594230f02ca3ee3f8b248df37b5d

--- project-system-gradle/src/com/android/tools/idea/gradle/project/build/invoker/GradleBuildInvokerImpl.kt
+++ project-system-gradle/src/com/android/tools/idea/gradle/project/build/invoker/GradleBuildInvokerImpl.kt
@@ -541,16 +541,20 @@ class GradleBuildInvokerImpl @NonInjectable @VisibleForTesting internal construc
     }
 
     override fun onStatusChange(event: ExternalSystemTaskNotificationEvent) {
-      when (event) {
-        is ExternalSystemBuildEvent -> buildEventDispatcher.onEvent(event.getId(), event.buildEvent)
-        is ExternalSystemTaskExecutionEvent -> buildEventDispatcher.onEvent(event.getId(), convert((event)))
+      if (startBuildEventPosted) {
+        when (event) {
+          is ExternalSystemBuildEvent -> buildEventDispatcher.onEvent(event.getId(), event.buildEvent)
+          is ExternalSystemTaskExecutionEvent -> buildEventDispatcher.onEvent(event.getId(), convert((event)))
+        }
       }
       super.onStatusChange(event)
     }
 
     override fun onTaskOutput(id: ExternalSystemTaskId, text: String, stdOut: Boolean) {
-      buildEventDispatcher.setStdOut(stdOut)
-      buildEventDispatcher.append(text)
+      if (startBuildEventPosted) {
+        buildEventDispatcher.setStdOut(stdOut)
+        buildEventDispatcher.append(text)
+      }
       super.onTaskOutput(id, text, stdOut)
     }
 
