bac8929fd03cf4cc6260f87cd52d0dae2ffa35d9
Madalin Valceleanu <vmadalin@google.com>
2025-11-17 12:02:41 +0000

    Introduce dedicated AndroidAnnotationSupport with IntelliJ 2025.3
    With IntelliJ 2025.3 a support for third-party nullability providers
    was introduced by commit ac013e30b2de0a05 as part of IDEA-372145.
    Because of this change, now, to obtain a default annotation to
    apply to PSIElement, the getDefaultAnnotation() uses the registered
    AnnotationPackageSupport.EP_NAME extensions where AndroidAnnotationSupport
    was also introduced. However, this annotation support must be
    prioritized for Android Studio but also ensuring that 'androidx.annotation'
    is prioritized as NullabilityAnnotation over 'android.annotation', since
    this last is already bundled in the androidSDK.jar.
    
    At the same time, the AndroidInferNullityAnnotationAction changed
    the nullability default depending if module depends on androidx or not,
    however, this isn't longer required since that method already checks it
    via JavaLibraryUtil.hasLibraryClass(module, annotation) taking into
    consideration the registered annotations order.
    
    Existing tests on AndroidInferNullityActionTest got adapted, while keeping
    the previous expected behaviour.
    
    Test: AndroidInferNullityActionTest
    Bug: 459397850
    Change-Id: Ifafbaa4546278230af91c5eee0dd01e5e9724bbc

--- android/src/com/android/tools/idea/startup/AndroidStudioInitializer.kt
+++ android/src/com/android/tools/idea/startup/AndroidStudioInitializer.kt
@@ -25,6 +25,7 @@ import com.android.tools.idea.flags.StudioFlags
 import com.android.tools.idea.res.StudioCodeVersionAdapter
 import com.android.tools.idea.sdk.IdeSdks
 import com.android.tools.idea.stats.AndroidStudioUsageTracker
+import com.intellij.codeInsight.annoPackages.AnnotationPackageSupport
 import com.intellij.concurrency.JobScheduler
 import com.intellij.ide.ApplicationInitializedListener
 import com.intellij.openapi.application.ApplicationInfo
@@ -34,6 +35,7 @@ import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.updateSettings.impl.pluginsAdvertisement.PluginSuggestionProvider
 import com.intellij.openapi.util.IntellijInternalApi
 import com.intellij.util.application
+import kotlin.jvm.javaClass
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import org.jetbrains.android.sdk.AndroidSdkUtils
@@ -76,6 +78,9 @@ class AndroidStudioInitializer(private val coroutineScope: CoroutineScope) : App
     setupAndroidSdkForTests()
     // these clutter the UX by suggesting paid JetBrains' products to users. As an example see b/409203679
     removePluginSuggestionProviderExtension()
+
+    // Replace the platform extension with studio AndroidAnnotationSupport while prioritizing it over others
+    removeAndroidAnnotationSupportExtension()
   }
 
   /** Sets up collection of Android Studio specific analytics.  */
@@ -106,5 +111,13 @@ class AndroidStudioInitializer(private val coroutineScope: CoroutineScope) : App
   private fun removePluginSuggestionProviderExtension() {
     val ep = application.extensionArea.getExtensionPoint<PluginSuggestionProvider>("com.intellij.pluginSuggestionProvider")
     ep.unregisterExtensions({ _, _ -> false }, false)
+  }
+
+  private fun removeAndroidAnnotationSupportExtension() {
+    AnnotationPackageSupport.EP_NAME.extensions
+      .find { it.javaClass.name == "com.intellij.codeInsight.annoPackages.AndroidAnnotationSupport"  }
+      ?.let {
+        AnnotationPackageSupport.EP_NAME.point.unregisterExtension(it::class.java)
+      }
   }
 }
