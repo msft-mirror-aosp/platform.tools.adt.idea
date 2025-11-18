7d281cec445431d6f826e5df9ca235a1f7d550ff
James Rose <rosej@google.com>
2025-11-18 14:33:04 -0800

    Default to cold boot for AI Glasses
    Bug: 461773579
    Test: AddDeviceWizardTest
    Change-Id: I9d50ab46d2945ce815f8dd82488cc822dc4d627f

--- avd/testSrc/com/android/tools/idea/avd/AddDeviceWizardTest.kt
+++ avd/testSrc/com/android/tools/idea/avd/AddDeviceWizardTest.kt
@@ -194,6 +194,7 @@ class AddDeviceWizardTest {
           AvdManager.parseIniFile(PathFileWrapper(avdFolder.resolve("config.ini")), null)
         )
       assertThat(properties[ConfigKey.LCD_TRANSPARENT]).isEqualTo("yes")
+      assertThat(properties[ConfigKey.FORCE_COLD_BOOT_MODE]).isEqualTo("yes")
 
       val userSettings = AvdManager.parseEnvironmentFile(avdFolder, null)
       assertThat(userSettings[EnvironmentKey.IMAGE])
