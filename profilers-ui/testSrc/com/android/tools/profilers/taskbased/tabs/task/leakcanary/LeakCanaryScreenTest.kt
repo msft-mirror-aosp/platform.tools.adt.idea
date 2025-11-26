321b9fb5f08dcdf9738aefda204f39aa007970c6
arvind-google <arvindtech@google.com>
2025-11-26 12:15:14 +0530

    Refactor LeakCanaryModel event handling
    Modify `leakDetected` to return early after a message is handled by
    `handleRetainedObject` or `handleAnalysisProgress`. Update helper
    functions to return a boolean indicating if the message was consumed.
    
    Bug: 463708958
    Bug: 461978098
    Test: NA
    Change-Id: Ibf8208e99cdbcefcf2c829dcb51ffb38d5c5ece0

--- profilers-ui/testSrc/com/android/tools/profilers/taskbased/tabs/task/leakcanary/LeakCanaryScreenTest.kt
+++ profilers-ui/testSrc/com/android/tools/profilers/taskbased/tabs/task/leakcanary/LeakCanaryScreenTest.kt
@@ -78,6 +78,7 @@ class LeakCanaryScreenTest : WithFakeTimer {
   @Test
   fun `test leak canary left and right panel data with the first selected leak`() {
     val analysis = getMultipleLeaksAnalysis()
+    Truth.assertThat(analysis).isNotNull()
     leakCanaryModel.addLeaks((analysis as AnalysisSuccess).leaks)
     leakCanaryModel.onLeakSelection(leakCanaryModel.leaks.value[0])
     composeTestRule.setContent { LeakCanaryScreen(leakCanaryModel = leakCanaryModel) }
@@ -126,6 +127,7 @@ class LeakCanaryScreenTest : WithFakeTimer {
   @Test
   fun `test leak canary left and right panel data changes on the selected leak changes`() {
     val analysis = getMultipleLeaksAnalysis()
+    Truth.assertThat(analysis).isNotNull()
     composeTestRule.setContent { LeakCanaryScreen(leakCanaryModel = leakCanaryModel) }
     // Initially, it's an empty leak list
     composeTestRule.onNodeWithText(TaskBasedUxStrings.LEAKCANARY_LEAK_HEADER_TEXT).isDisplayed()
@@ -165,7 +167,7 @@ class LeakCanaryScreenTest : WithFakeTimer {
     composeTestRule.onNodeWithText("No").isDisplayed()
   }
 
-  private fun getMultipleLeaksAnalysis(): Analysis {
+  private fun getMultipleLeaksAnalysis(): Analysis? {
     val analysis =
       """
         ====================================
@@ -236,6 +238,7 @@ class LeakCanaryScreenTest : WithFakeTimer {
   @Test
   fun  `test leak canary left and right panel data with leaks having className in multiple lines`() {
     val analysis = getLeaksAnalysisWithClassNameInMultipleLine()
+    Truth.assertThat(analysis).isNotNull()
     composeTestRule.setContent {
       LeakCanaryScreen(leakCanaryModel = leakCanaryModel)
     }
@@ -263,7 +266,7 @@ class LeakCanaryScreenTest : WithFakeTimer {
     composeTestRule.onNodeWithText("Unknown").isDisplayed()
   }
 
-  private fun getLeaksAnalysisWithClassNameInMultipleLine(): Analysis {
+  private fun getLeaksAnalysisWithClassNameInMultipleLine(): Analysis? {
     val analysis = """
         ====================================
         HEAP ANALYSIS RESULT
@@ -424,6 +427,7 @@ class LeakCanaryScreenTest : WithFakeTimer {
   @Test
   fun `test expand and collapse all via buttons`() {
     val analysis = getMultipleLeaksAnalysis()
+    Truth.assertThat(analysis).isNotNull()
     leakCanaryModel.addLeaks((analysis as AnalysisSuccess).leaks)
     leakCanaryModel.onLeakSelection(leakCanaryModel.leaks.value[0])
     composeTestRule.setContent { LeakCanaryScreen(leakCanaryModel = leakCanaryModel) }
@@ -453,6 +457,7 @@ class LeakCanaryScreenTest : WithFakeTimer {
   @Test
   fun `test expand and collapse all via shortcuts`() {
     val analysis = getMultipleLeaksAnalysis()
+    Truth.assertThat(analysis).isNotNull()
     leakCanaryModel.addLeaks((analysis as AnalysisSuccess).leaks)
     leakCanaryModel.onLeakSelection(leakCanaryModel.leaks.value[0])
     composeTestRule.setContent { LeakCanaryScreen(leakCanaryModel = leakCanaryModel) }
@@ -491,6 +496,7 @@ class LeakCanaryScreenTest : WithFakeTimer {
   @Test
   fun `clicking copy to clipboard button should capture the correct leak string`() {
     val analysis = getMultipleLeaksAnalysis()
+    Truth.assertThat(analysis).isNotNull()
     val selectedLeak = (analysis as AnalysisSuccess).leaks[0]
     leakCanaryModel.addLeaks(analysis.leaks)
     leakCanaryModel.onLeakSelection(selectedLeak)
