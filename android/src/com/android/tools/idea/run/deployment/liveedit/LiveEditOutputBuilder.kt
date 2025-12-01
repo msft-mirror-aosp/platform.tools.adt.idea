d8ca22ec3e6c5475f57aa3ccf0ea95060e20941e
Amaury Medeiros <amaurym@google.com>
2025-12-01 14:38:03 +0000

    Remove println debug logs from liveEditOutputBuilder
    Bug: N/A
    Test: N/A
    Change-Id: Id235243972732bb18007e2bd3a223031fe4c9ade

--- android/src/com/android/tools/idea/run/deployment/liveedit/LiveEditOutputBuilder.kt
+++ android/src/com/android/tools/idea/run/deployment/liveedit/LiveEditOutputBuilder.kt
@@ -172,10 +172,8 @@ internal class LiveEditOutputBuilder(val unrestricted: Boolean = false) {
 
     val metadata = parseMetadata(newClass)
     val classType = if (metadata is KotlinClassMetadata.SyntheticClass) {
-      println("support: " + newClass.name)
       LiveEditClassType.SUPPORT_CLASS
     } else {
-      println("normal: " + newClass.name)
       LiveEditClassType.NORMAL_CLASS
     }
 
