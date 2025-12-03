9f855c353f187db0edacf643b93fcb8e5f6af1ce
Paul Milian <paulmilian@google.com>
2025-12-03 14:58:48 +0000

    Fix flaky test
    Fixes: 465143895
    Test: bazel test --config=remote\
     //tools/adt/idea/wear-dwf:intellij.android.wear-dwf.tests_tests\
     --runs_per_test=2000
    Change-Id: I20e176b95daf711fa84d16cd5633e2c07ac7ebab

--- wear-dwf/testSrc/com/android/tools/idea/wear/dwf/inspections/FeatureRequiresHigherWFFVersionInspectionTest.kt
+++ wear-dwf/testSrc/com/android/tools/idea/wear/dwf/inspections/FeatureRequiresHigherWFFVersionInspectionTest.kt
@@ -25,6 +25,14 @@ import com.android.tools.wear.wff.WFFVersion.WFFVersion3
 import com.android.tools.wear.wff.WFFVersion.WFFVersion4
 import com.google.common.truth.Truth.assertThat
 import com.intellij.lang.annotation.HighlightSeverity
+import com.intellij.openapi.application.edtWriteAction
+import com.intellij.openapi.application.invokeAndWaitIfNeeded
+import com.intellij.openapi.application.runInEdt
+import com.intellij.openapi.application.runWriteAction
+import com.intellij.psi.impl.source.xml.XmlDocumentImpl
+import com.intellij.psi.xml.XmlFile
+import com.intellij.testFramework.runInEdtAndWait
+import com.intellij.util.FileContentUtilCore
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -176,6 +184,12 @@ class FeatureRequiresHigherWFFVersionInspectionTest {
 
     // Once we use version 2, there should no longer be any errors
     overrideCurrentWFFVersion(WFFVersion2, projectRule.testRootDisposable)
+    // This will be called by RawWatchFaceXmlSchemaUpdater in production
+    runInEdtAndWait {
+      runWriteAction {
+        FileContentUtilCore.reparseFiles(watchFaceFile.virtualFile)
+      }
+    }
     fixture.configureFromExistingVirtualFile(watchFaceFile.virtualFile)
 
     errors = fixture.doHighlighting().filter { it.severity == HighlightSeverity.ERROR }
