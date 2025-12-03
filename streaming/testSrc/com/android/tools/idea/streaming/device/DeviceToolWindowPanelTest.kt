ffd65a277fdd3f26ee34a6e0718513c4568d9823
Sergey Prigogin <sprigogin@google.com>
2025-12-02 18:32:23 -0800

    Enable two embedded emulator feature flags internally
    The enabled flags:
    - Allow AI glasses AVD to run embedded
    - Make zoom and XR toolbars collapsible
    
    Bug: N/A
    Test: existing
    Change-Id: I8f30ba3723c538143f2ab8270098ea637614eb27

--- streaming/testSrc/com/android/tools/idea/streaming/device/DeviceToolWindowPanelTest.kt
+++ streaming/testSrc/com/android/tools/idea/streaming/device/DeviceToolWindowPanelTest.kt
@@ -37,7 +37,7 @@ import com.android.tools.idea.streaming.actions.ToggleFloatingXrToolbarAction
 import com.android.tools.idea.streaming.core.AbstractDisplayView
 import com.android.tools.idea.streaming.core.DeviceDisplayListener
 import com.android.tools.idea.streaming.core.DisplayType
-import com.android.tools.idea.streaming.core.FloatingToolbarContainer
+import com.android.tools.idea.streaming.core.expandFloatingToolbar
 import com.android.tools.idea.streaming.device.AndroidKeyEventActionType.ACTION_DOWN
 import com.android.tools.idea.streaming.device.AndroidKeyEventActionType.ACTION_DOWN_AND_UP
 import com.android.tools.idea.streaming.device.AndroidKeyEventActionType.ACTION_UP
@@ -361,7 +361,7 @@ class DeviceToolWindowPanelTest {
     waitForCondition(10.seconds) { agent.isRunning && panel.isConnected }
     waitForFrame()
 
-    expandFloatingToolbar()
+    fakeUi.expandFloatingToolbar()
     fakeUi.mouseClickOn(fakeUi.getComponent<ActionButton> { it.action.templateText == "View Direction" })
 
     val xrInputController = DeviceXrInputController.getInstance(project, displayView.deviceClient)
@@ -423,7 +423,7 @@ class DeviceToolWindowPanelTest {
     fakeUi.keyboard.release(VK_E)
     assertThat(getNextControlMessageAndWaitForFrame()).isEqualTo(XrVelocityMessage(-1f, -1f, 0f))
 
-    expandFloatingToolbar()
+    fakeUi.expandFloatingToolbar()
     fakeUi.mouseClickOn(fakeUi.getComponent<ActionButton> { it.action.templateText == "Interact with Apps" })
     // Switching to Interact with Apps resets state of the navigation keys.
     assertThat(getNextControlMessageAndWaitForFrame()).isEqualTo(XrVelocityMessage(0f, 0f, 0f))
@@ -442,7 +442,7 @@ class DeviceToolWindowPanelTest {
     waitForCondition(10.seconds) { agent.isRunning && panel.isConnected }
     waitForFrame()
 
-    expandFloatingToolbar()
+    fakeUi.expandFloatingToolbar()
     fakeUi.mouseClickOn(fakeUi.getComponent<ActionButton> { it.action.templateText == "View Direction" })
 
     val xrInputController = DeviceXrInputController.getInstance(project, displayView.deviceClient)
@@ -470,7 +470,7 @@ class DeviceToolWindowPanelTest {
     waitForCondition(10.seconds) { agent.isRunning && panel.isConnected }
     waitForFrame()
 
-    expandFloatingToolbar()
+    fakeUi.expandFloatingToolbar()
     fakeUi.mouseClickOn(fakeUi.getComponent<ActionButton> { it.action.templateText == "Move Right/Left and Up/Down" })
 
     val xrInputController = DeviceXrInputController.getInstance(project, displayView.deviceClient)
@@ -493,7 +493,7 @@ class DeviceToolWindowPanelTest {
     fakeUi.mouse.wheel(10, 100, -3)
     assertThat(getNextControlMessageAndWaitForFrame().toString()).isEqualTo("XrTranslationMessage(x = 0.0, y = 0.0, z = -0.25)")
 
-    expandFloatingToolbar()
+    fakeUi.expandFloatingToolbar()
     fakeUi.mouseClickOn(fakeUi.getComponent<ActionButton> { it.action.templateText == "Move Forward/Backward" })
     assertThat(xrInputController.inputMode).isEqualTo(XrInputMode.LOCATION_IN_SPACE_Z)
     assertThat(project.service<HardwareInputStateStorage>().isHardwareInputEnabled(displayView.deviceId)).isFalse()
@@ -773,16 +773,6 @@ class DeviceToolWindowPanelTest {
   private fun renderAndGetFrameNumber(displayId: Int = PRIMARY_DISPLAY_ID): UInt {
     fakeUi.render() // The frame number may get updated as a result of rendering.
     return panel.findDisplayView(displayId)!!.frameNumber
-  }
-
-  private fun expandFloatingToolbar() {
-    fakeUi.layoutAndDispatchEvents()
-    val toolbar = fakeUi.getComponent<FloatingToolbarContainer>()
-    // Trigger expansion of the floating toolbar.
-    fakeUi.mouse.moveTo(toolbar.locationOnScreen.x + toolbar.width / 2, toolbar.locationOnScreen.y + toolbar.height - toolbar.width / 2)
-    fakeUi.layoutAndDispatchEvents()
-    waitForCondition(1.seconds) { toolbar.activationFactor == 1.0 }
-    fakeUi.layoutAndDispatchEvents()
   }
 
   @Suppress("SameParameterValue")
