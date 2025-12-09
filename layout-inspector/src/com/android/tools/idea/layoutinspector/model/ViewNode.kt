56f385de409bbd3f053a512f9a188f2ca96595d7
Jens Ole Lauridsen <jlauridsen@google.com>
2025-12-09 08:15:44 -0800

    Read isWebView from view inspector
    Bug: 466127131
    Test: N/A
    Change-Id: If1dc1d251ef31bdea9a84c6a76dada5d440200da

--- layout-inspector/src/com/android/tools/idea/layoutinspector/model/ViewNode.kt
+++ layout-inspector/src/com/android/tools/idea/layoutinspector/model/ViewNode.kt
@@ -52,6 +52,7 @@ private val systemPackagePrefixes =
  * @param viewId the id set by the developer in the View.id attribute
  * @param textValue the text value if present
  * @param layoutFlags flags from WindowManager.LayoutParams
+ * @param isDerivedFromWebView if the View class is extending android.webkit.WebView
  */
 open class ViewNode(
   var drawId: Long,
@@ -62,6 +63,7 @@ open class ViewNode(
   var viewId: ResourceReference?,
   var textValue: String,
   var layoutFlags: Int,
+  var isDerivedFromWebView: Boolean,
 ) {
   @TestOnly
   constructor(
@@ -72,6 +74,7 @@ open class ViewNode(
     viewId: ResourceReference?,
     textValue: String,
     layoutFlags: Int,
+    isDerivedFromWebView: Boolean,
   ) : this(
     drawId,
     qualifiedName,
@@ -81,12 +84,23 @@ open class ViewNode(
     viewId,
     textValue,
     layoutFlags,
+    isDerivedFromWebView,
   )
 
   /** constructor for synthetic nodes */
   constructor(
     qualifiedName: String
-  ) : this(-1, qualifiedName, null, Rectangle(), Rectangle(), null, "", 0)
+  ) : this(
+    -1,
+    qualifiedName,
+    null,
+    Rectangle(),
+    Rectangle(),
+    null,
+    "",
+    0,
+    isDerivedFromWebView = false,
+  )
 
   @Suppress("LeakingThis") val treeNode = TreeViewNode(this)
 
