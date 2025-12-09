489e837aa5cce76d1156b1b9fb6ed4105bc02f87
Jens Ole Lauridsen <jlauridsen@google.com>
2025-12-09 12:30:48 -0800

    Refactoring: Make SelectedViewModel use the selected View
    Instead of fabricating properties to display: name, id, icon.
    
    Bug: 466127131
    Test: SelectedViewModelTest
    Change-Id: Ic3cabcbb4f5b295def4377c631e0e50b1c8a7e0e

--- layout-inspector/src/com/android/tools/idea/layoutinspector/model/IconProvider.kt
+++ layout-inspector/src/com/android/tools/idea/layoutinspector/model/IconProvider.kt
@@ -39,8 +39,9 @@ const val ROOT_NAME = "root"
 
 object IconProvider {
 
-  fun getIconForView(qualifiedName: String, isCompose: Boolean): Icon =
-    if (isCompose) getIconForComposeViewNode(qualifiedName) else getIconForViewNode(qualifiedName)
+  fun getIconForView(view: ViewNode): Icon =
+    if (view is ComposeViewNode) getIconForComposeViewNode(view.qualifiedName)
+    else getIconForViewNode(view.qualifiedName)
 
   private fun getIconForViewNode(viewName: String): Icon {
     // Remove "AppCompat" and "Material" prefixes from the simple tag name such that we get
