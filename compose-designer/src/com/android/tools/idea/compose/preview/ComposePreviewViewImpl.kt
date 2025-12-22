5385eb4cabf3415f2a3f42f75e57e9d8b4b10a34
Diego Perez <diegoperez@google.com>
2025-12-22 13:25:17 +0000

    Avoid error when psiFilePointer.virtualFile is null
    Adding some additional guards for the cases where virtualFile is
    null.
    
    Test: N/A
    Fixes: 470928106
    Fixes: 470226067
    Change-Id: Id1e9b2acf29821f935ad72fd2447182f090f3520

--- compose-designer/src/com/android/tools/idea/compose/preview/ComposePreviewViewImpl.kt
+++ compose-designer/src/com/android/tools/idea/compose/preview/ComposePreviewViewImpl.kt
@@ -45,6 +45,7 @@ import com.intellij.openapi.actionSystem.DataSink
 import com.intellij.openapi.actionSystem.UiDataProvider
 import com.intellij.openapi.application.EDT
 import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.extensions.ExtensionPointName
 import com.intellij.openapi.fileEditor.FileEditor
 import com.intellij.openapi.project.DumbService
@@ -327,7 +328,15 @@ internal class ComposePreviewViewImpl(
       if (Disposer.isDisposed(workbench) || project.isDisposed || !parentEditor.isValid)
         return@invokeLaterIfNeeded
 
-      notificationPanel.updateNotifications(psiFilePointer.virtualFile, parentEditor, project)
+      val vFile = psiFilePointer.virtualFile
+      if (vFile == null) {
+        thisLogger()
+          .warn(
+            "virtualFile is null for $psiFilePointer element=${psiFilePointer.element} file=${psiFilePointer.containingFile}"
+          )
+      } else {
+        notificationPanel.updateNotifications(vFile, parentEditor, project)
+      }
     }
 
   /** Method called to ask all notifications to update. */
