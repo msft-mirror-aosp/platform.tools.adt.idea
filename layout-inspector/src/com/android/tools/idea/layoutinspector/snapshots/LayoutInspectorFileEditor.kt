e6980cfea273566a99b0ba055e1bfabb63abcb4f
Pierfrancesco Soffritti <psoffritti@google.com>
2026-01-16 14:32:37 +0000

    Throw an error when the picture type is unknown
    Test: NA
    Bug: 476265440
    Change-Id: I184fbaa633ee4fdf408746bdad9b5675ad216ed1

--- layout-inspector/src/com/android/tools/idea/layoutinspector/snapshots/LayoutInspectorFileEditor.kt
+++ layout-inspector/src/com/android/tools/idea/layoutinspector/snapshots/LayoutInspectorFileEditor.kt
@@ -155,28 +155,24 @@ class LayoutInspectorFileEditor(val project: Project, private val path: Path) :
           treeSettings = treeSettings,
         )
 
-      val hasBitmapImage =
-        when (model.pictureType) {
-          AndroidWindow.ImageType.BITMAP_AS_REQUESTED -> true
-          AndroidWindow.ImageType.UNKNOWN,
-          AndroidWindow.ImageType.SKP_PENDING,
-          AndroidWindow.ImageType.SKP -> false
-        }
-
       rootPanel =
-        if (hasBitmapImage) {
-          createNewLayoutInspectorUi(this, project, layoutInspector)
-        } else {
-          // TODO(b/459411932): remove this else statement once we fully remove the old inspector
-          // Notify the user that skia images are not going to be supported going forward
-          notificationModel.addNotification(
-            id = SNAPSHOT_OUTDATED_ID,
-            text = LayoutInspectorBundle.message(SNAPSHOT_OUTDATED_ID),
-            sticky = true,
-          )
+        when (model.pictureType) {
+          AndroidWindow.ImageType.BITMAP_AS_REQUESTED ->
+            createNewLayoutInspectorUi(this, project, layoutInspector)
+          AndroidWindow.ImageType.SKP_PENDING,
+          AndroidWindow.ImageType.SKP -> {
+            // TODO(b/459411932): remove this else statement once we fully remove the old inspector
+            // Notify the user that skia images are not going to be supported going forward
+            notificationModel.addNotification(
+              id = SNAPSHOT_OUTDATED_ID,
+              text = LayoutInspectorBundle.message(SNAPSHOT_OUTDATED_ID),
+              sticky = true,
+            )
 
-          // If the snapshot file contains a skia image fallback to old layout inspector
-          createOldLayoutInspectorUi(this, project, layoutInspector)
+            // If the snapshot file contains a skia image fallback to old layout inspector
+            createOldLayoutInspectorUi(this, project, layoutInspector)
+          }
+          AndroidWindow.ImageType.UNKNOWN -> throw IllegalStateException("Unknown picture type")
         }
 
       metadata.loadDuration = System.currentTimeMillis() - startTime
@@ -199,7 +195,7 @@ class LayoutInspectorFileEditor(val project: Project, private val path: Path) :
           override fun isStatusVisible() = true
         }
       status.appendLine("Error loading snapshot")
-      (exception as? SnapshotLoaderException)?.message?.let { status.appendLine(it) }
+      exception.message?.let { status.appendLine(it) }
 
       return object : JPanel() {
         init {
