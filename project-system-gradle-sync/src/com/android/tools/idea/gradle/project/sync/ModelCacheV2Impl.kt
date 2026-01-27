e5adf09ef055f4c838871d7969bb44725fd0d033
Luke Edgar <lukeedgar@google.com>
2026-01-27 14:57:45 +0000

    [privacy sandbox] Remove PrivacySandboxInfo from adt/idea
    Bug: 455530558
    Test: N/a
    Change-Id: I5392632b9365d146e2385c08438d3890832444c4

--- project-system-gradle-sync/src/com/android/tools/idea/gradle/project/sync/ModelCacheV2Impl.kt
+++ project-system-gradle-sync/src/com/android/tools/idea/gradle/project/sync/ModelCacheV2Impl.kt
@@ -109,7 +109,6 @@ import com.android.tools.idea.gradle.model.impl.IdeModuleWellKnownSourceSet
 import com.android.tools.idea.gradle.model.impl.IdeModuleWellKnownSourceSet.MAIN
 import com.android.tools.idea.gradle.model.impl.IdeModuleWellKnownSourceSet.TEST_FIXTURES
 import com.android.tools.idea.gradle.model.impl.IdeMultiVariantDataImpl
-import com.android.tools.idea.gradle.model.impl.IdePrivacySandboxSdkInfoImpl
 import com.android.tools.idea.gradle.model.impl.IdeProductFlavorContainerImpl
 import com.android.tools.idea.gradle.model.impl.IdeProductFlavorImpl
 import com.android.tools.idea.gradle.model.impl.IdeProjectPathImpl
@@ -944,19 +943,6 @@ fun modelCacheV2Impl(
       buildInformation = buildTasksOutputInformationFrom(artifact),
       codeShrinker = convertCodeShrinker(artifact.codeShrinker),
       isTestArtifact = isMainArtifactInTestProject || name == IdeArtifactName.ANDROID_TEST || name == IdeArtifactName.TEST_FIXTURES,
-      privacySandboxSdkInfo =
-        if (modelVersions[ModelFeature.HAS_PRIVACY_SANDBOX_SDK_INFO])
-          artifact.privacySandboxSdkInfo?.let {
-            IdePrivacySandboxSdkInfoImpl(
-              it.task,
-              it.outputListingFile,
-              it.additionalApkSplitTask,
-              it.additionalApkSplitFile,
-              it.taskLegacy,
-              it.outputListingLegacyFile,
-            )
-          }
-        else null,
       desugaredMethodsFiles = getDesugaredMethodsList(artifact, fallbackDesugaredMethodsFiles).toList(),
       generatedClassPaths = if (modelVersions[ModelFeature.HAS_GENERATED_CLASSPATHS]) artifact.generatedClassPaths else emptyMap(),
       bytecodeTransforms =
