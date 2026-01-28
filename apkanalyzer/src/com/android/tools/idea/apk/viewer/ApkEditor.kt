a844142ce5b70ee8b08f66e0ddcffba226197224
Alon Albert <aalbert@google.com>
2026-01-28 09:21:16 -0800

    Pretty Print JSON Files
    Bug: NA
    Test: Added
    Change-Id: I13c2e3ba2895f7c0d8364c82c4292a9b6cb9597d

--- apkanalyzer/src/com/android/tools/idea/apk/viewer/ApkEditor.kt
+++ apkanalyzer/src/com/android/tools/idea/apk/viewer/ApkEditor.kt
@@ -34,6 +34,8 @@ import com.android.tools.idea.log.LogWrapper
 import com.android.tools.instrumentation.threading.agent.callback.ThreadingCheckerUtil
 import com.android.tools.proguard.ProguardMap
 import com.android.utils.FileUtils
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonObject
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.fileChooser.FileChooser
@@ -60,7 +62,6 @@ import com.intellij.openapi.vfs.newvfs.BulkFileListener
 import com.intellij.openapi.vfs.newvfs.events.VFileEvent
 import com.intellij.ui.JBSplitter
 import com.intellij.ui.components.JBLabel
-import org.jetbrains.annotations.VisibleForTesting
 import java.beans.PropertyChangeListener
 import java.io.File
 import java.io.IOException
@@ -71,8 +72,10 @@ import java.security.NoSuchAlgorithmException
 import java.util.Optional
 import javax.swing.JComponent
 import javax.swing.LayoutFocusTraversalPolicy
+import kotlin.io.path.extension
 import kotlin.io.path.name
 import kotlin.math.max
+import org.jetbrains.annotations.VisibleForTesting
 
 internal class ApkEditor(
   private val project: Project,
@@ -382,6 +385,11 @@ internal class ApkEditor(
         null -> ApkVirtualFile.create(p, content)
         else -> ApkVirtualFile.createText(p, text)
       }
+    }
+    if (p.extension == "json") {
+      val gson = GsonBuilder().setPrettyPrinting().create()
+      val jsonObject = gson.fromJson(String(content), JsonObject::class.java)
+      return ApkVirtualFile.createText(p, gson.toJson(jsonObject))
     }
 
     val file = JarFileSystem.getInstance().findFileByPath(archive.getPath().toString())
