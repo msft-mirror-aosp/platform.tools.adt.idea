731264554345e7c61e98b326c620d06121192fcc
Paul Milian <paulmilian@google.com>
2026-01-29 16:38:38 +0000

    Disable default make task for wear run configurations
    Fixes: b/476891944
    Test: added regression tests
    Change-Id: I45c85253ad1afe09c9c2c5608a7ac3f4993e2e1a

--- android/run/testSrc/com/android/tools/idea/run/configuration/AndroidDeclarativeWatchFaceConfigurationTypeTest.kt
+++ android/run/testSrc/com/android/tools/idea/run/configuration/AndroidDeclarativeWatchFaceConfigurationTypeTest.kt
@@ -19,20 +19,26 @@ import com.android.flags.junit.FlagRule
 import com.android.tools.idea.flags.StudioFlags
 import com.android.tools.idea.testing.flags.overrideForTest
 import com.google.common.truth.Truth.assertThat
+import com.intellij.compiler.options.CompileStepBeforeRun
+import com.intellij.execution.RunManager
 import com.intellij.execution.configurations.ConfigurationType
 import com.intellij.openapi.extensions.ExtensionNotApplicableException
 import com.intellij.testFramework.ApplicationRule
 import com.intellij.testFramework.DisposableRule
+import com.intellij.testFramework.ProjectRule
 import com.intellij.testFramework.RuleChain
 import org.junit.Rule
 import org.junit.Test
 
 class AndroidDeclarativeWatchFaceConfigurationTypeTest {
   private val flagRule = FlagRule(StudioFlags.WEAR_DECLARATIVE_WATCH_FACE_RUN_CONFIGURATION, true)
-  private val appRule = ApplicationRule()
+  private val projectRule = ProjectRule()
   private val disposableRule = DisposableRule()
 
-  @get:Rule val rule = RuleChain(flagRule, appRule, disposableRule)
+  @get:Rule val rule = RuleChain(flagRule, projectRule, disposableRule)
+
+  private val project
+    get() = projectRule.project
 
   @Test
   fun `the declarative watch face run configuration is available when the flag is enabled`() {
@@ -45,5 +51,19 @@ class AndroidDeclarativeWatchFaceConfigurationTypeTest {
     StudioFlags.WEAR_DECLARATIVE_WATCH_FACE_RUN_CONFIGURATION.overrideForTest(false, disposableRule.disposable)
 
     AndroidDeclarativeWatchFaceConfigurationType()
+  }
+
+  // Regression test for b/476891944
+  @Test
+  fun `default make task is disabled`() {
+    val factory = AndroidDeclarativeWatchFaceConfigurationType().configurationFactories.single()
+    val configSettings = RunManager.getInstance(project)
+      .createConfiguration("test config", factory)
+    val beforeRunTasks = configSettings.configuration.beforeRunTasks
+
+    val makeTasks = beforeRunTasks
+      .filterIsInstance<CompileStepBeforeRun.MakeBeforeRunTask>()
+      .filter { it.isEnabled }
+    assertThat(makeTasks).isEmpty()
   }
 }
