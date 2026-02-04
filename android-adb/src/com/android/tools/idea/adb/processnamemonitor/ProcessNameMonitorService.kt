6cdf24d8beea2c35091f48454d840f01bd3165a5
Alexander Demurov <ademurov@google.com>
2026-02-04 09:40:53 -0800

    Removed launched `processnamemonitor` flags
    Test: existing
    Bug: n/a
    Change-Id: I395588c7477a311cd2937b44ebe5d6bbf5b8b2f4

--- android-adb/src/com/android/tools/idea/adb/processnamemonitor/ProcessNameMonitorService.kt
+++ android-adb/src/com/android/tools/idea/adb/processnamemonitor/ProcessNameMonitorService.kt
@@ -49,11 +49,7 @@ internal class ProcessNameMonitorService(project: Project) : ProcessNameMonitor,
     val pollingIntervalMs = StudioFlags.PROCESS_NAME_TRACKER_AGENT_INTERVAL_MS.get()
     // If Logcat Proto format is supported and enabled, we don't need to use an agent.
     val shouldUseAgentForSdk: (Int) -> Boolean = { sdk -> sdk < LOGCAT_PROTO_SUPPORT_SDK || !StudioFlags.LOGCAT_PROTOBUF_ENABLED.get() }
-    val trackerAgentConfig =
-      when (StudioFlags.PROCESS_NAME_TRACKER_AGENT_ENABLE.get()) {
-        true -> AgentProcessTrackerConfig(getAgentPath(), pollingIntervalMs, shouldUseAgentForSdk)
-        false -> null
-      }
+    val trackerAgentConfig = AgentProcessTrackerConfig(getAgentPath(), pollingIntervalMs, shouldUseAgentForSdk)
     val config = ProcessNameMonitor.Config(StudioFlags.PROCESS_NAME_MONITOR_MAX_RETENTION.get(), trackerAgentConfig)
 
     ProcessNameMonitorImpl.create(parentScope, adbSession, deviceProvisioner, config, adbLogger)
