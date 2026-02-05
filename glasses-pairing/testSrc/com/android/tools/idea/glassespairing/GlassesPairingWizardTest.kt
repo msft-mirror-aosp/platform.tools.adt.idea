35f176346c8db62cdd533c4dbd0a9bfc631ef4e9
Mike Moroney <mmoro@google.com>
2026-02-05 12:51:25 -0800

    Remove generic parameter from UsageTrackerWriter
    This is no longer needed for the upcoming logged
    in usage tracker writer.
    
    Bug: 438541344
    Test: Updated existing tests, no behavior change
    Change-Id: I94fab6c87ee7122d74b34f30c9b14ab7f6b600fe

--- glasses-pairing/testSrc/com/android/tools/idea/glassespairing/GlassesPairingWizardTest.kt
+++ glasses-pairing/testSrc/com/android/tools/idea/glassespairing/GlassesPairingWizardTest.kt
@@ -37,6 +37,7 @@ import com.android.tools.idea.glassespairing.LaunchState.Booting
 import com.android.tools.idea.glassespairing.LaunchState.Launching
 import com.android.tools.idea.glassespairing.LaunchState.Ready
 import com.google.common.truth.Truth.assertThat
+import com.google.protobuf.Message.Builder
 import com.google.wireless.android.play.playlog.proto.ClientAnalytics
 import com.google.wireless.android.sdk.stats.AndroidStudioEvent
 import com.google.wireless.android.sdk.stats.GlassesPairingEvent
@@ -401,15 +402,17 @@ private class TestWizardController : WizardController {
   }
 }
 
-private class TestTracker : UsageTrackerWriter<AndroidStudioEvent.Builder>() {
+private class TestTracker : UsageTrackerWriter() {
   val events = CopyOnWriteArrayList<GlassesPairingEvent.EventKind>()
 
   override fun logDetails(logEvent: ClientAnalytics.LogEvent.Builder) {}
 
-  override fun processMessage(eventTimeMs: Long, studioEvent: AndroidStudioEvent.Builder) {
+  override fun processEvent(studioEvent: AndroidStudioEvent.Builder): Builder {
     if (studioEvent.kind == AndroidStudioEvent.EventKind.GLASSES_PAIRING_EVENT) {
       events.add(studioEvent.glassesPairingEvent.kind)
     }
+
+    return studioEvent
   }
 
   override fun close() {}
