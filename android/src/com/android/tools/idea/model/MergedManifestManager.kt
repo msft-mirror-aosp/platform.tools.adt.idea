d59f114f0d8f99c86a03c8c6c2afec789689930c
Christophe Rhodes <xof@google.com>
2026-02-10 18:13:25 +0000

    Rewrite AndroidLint's getMergedManifest
    Bypass MergedManifestManager to implement our own, very simple
    caching.  This does mean we need to open some more of the guts of
    MergedManifestManager / MergedManifestInfo in order to be able to
    thread through our own recursive snapshot getter, rather than
    hard-coding the use of MergedManifestManager.get...(...) in the
    recursive manifest merge call.
    
    Bug: 472799724
    Test: an excellent coverage of existing tests
    Change-Id: I48522380fb308d92467e6010eb971522c4ee6c21

--- android/src/com/android/tools/idea/model/MergedManifestManager.kt
+++ android/src/com/android/tools/idea/model/MergedManifestManager.kt
@@ -67,7 +67,7 @@ private val RECOMPUTE_INTERVAL = 50.milliseconds.toJavaDuration()
  * @see [getOrCreateSnapshotInCallingThread]
  * @see [MergedManifestManager.getSnapshot]
  */
-private class MergedManifestSupplier(private val module: Module) : AsyncSupplier<MergedManifestSnapshot>, Disposable, ModificationTracker {
+class MergedManifestSupplier(private val module: Module) : AsyncSupplier<MergedManifestSnapshot>, Disposable, ModificationTracker {
 
   private val delegate = ThrottlingAsyncSupplier(::getOrCreateSnapshotFromDelegate, ::snapshotUpToDate, RECOMPUTE_INTERVAL)
 
@@ -105,7 +105,10 @@ private class MergedManifestSupplier(private val module: Module) : AsyncSupplier
   override fun get(): ListenableFuture<MergedManifestSnapshot> = delegate.get()
 
   @Slow
-  private fun getOrCreateSnapshot(cachedSnapshot: MergedManifestSnapshot?): MergedManifestSnapshot {
+  fun getOrCreateSnapshot(
+    cachedSnapshot: MergedManifestSnapshot?,
+    recursiveSnapshotGetter: (Module) -> MergedManifestSnapshot = { MergedManifestManager.getFreshSnapshotInCallingThread(it) },
+  ): MergedManifestSnapshot {
     return runCancellableReadAction {
       val facet =
         module.androidFacet ?: throw IllegalArgumentException("Attempt to obtain manifest info from a non Android module: ${module.name}")
@@ -113,14 +116,17 @@ private class MergedManifestSupplier(private val module: Module) : AsyncSupplier
         // Make sure the module wasn't disposed while we were waiting for the read lock.
         facet.isDisposed || module.project.isDisposed -> throw ProcessCanceledException()
         cachedSnapshot != null && snapshotUpToDate(cachedSnapshot) -> cachedSnapshot
-        else -> createMergedManifestSnapshot(facet)
+        else -> createMergedManifestSnapshot(facet, recursiveSnapshotGetter)
       }
     }
   }
 
   /** Create a [MergedManifestSnapshot] and record associated telemetry. */
   @Slow
-  private fun createMergedManifestSnapshot(facet: AndroidFacet): MergedManifestSnapshot {
+  private fun createMergedManifestSnapshot(
+    facet: AndroidFacet,
+    recursiveSnapshotGetter: (Module) -> MergedManifestSnapshot,
+  ): MergedManifestSnapshot {
     val timeMarker = timeSource.markNow()
     val token = Object()
     ApplicationManager.getApplication().messageBus.syncPublisher(MergedManifestSnapshotComputeListener.TOPIC).snapshotCreationStarted(token)
@@ -129,7 +135,7 @@ private class MergedManifestSupplier(private val module: Module) : AsyncSupplier
     var result = MergeResult.FAILED
 
     try {
-      snapshot = MergedManifestSnapshotFactory.createMergedManifestSnapshot(facet)
+      snapshot = MergedManifestSnapshotFactory.createMergedManifestSnapshot(facet, recursiveSnapshotGetter)
       result = MergeResult.SUCCESS
     } catch (e: ProcessCanceledException) {
       result = MergeResult.CANCELED
@@ -264,7 +270,7 @@ private class MergedManifestSupplier(private val module: Module) : AsyncSupplier
    * calculated in the calling thread for some legacy caller.
    */
   @AnyThread
-  private fun snapshotUpToDate(snapshot: MergedManifestSnapshot): Boolean {
+  fun snapshotUpToDate(snapshot: MergedManifestSnapshot): Boolean {
     // The only way the snapshot's merged manifest info could be null is if the facet
     // is disposed, in which case there's no need to try and recalculate it.
     val mergedManifestInfo = snapshot.mergedManifestInfo ?: return true
@@ -290,7 +296,7 @@ private class MergedManifestSupplier(private val module: Module) : AsyncSupplier
  * This class is open for mocking. Do not extend it.
  */
 class MergedManifestManager(module: Module) : Disposable {
-  private val supplier = MergedManifestSupplier(module)
+  val supplier = MergedManifestSupplier(module)
   val mergedManifest: AsyncSupplier<MergedManifestSnapshot>
     get() = supplier
 
--- android/src/com/android/tools/idea/model/MergedManifestSnapshotFactory.java
+++ android/src/com/android/tools/idea/model/MergedManifestSnapshotFactory.java
@@ -72,6 +72,7 @@ import com.intellij.openapi.util.text.StringUtil;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Set;
+import java.util.function.Function;
 import org.jetbrains.android.facet.AndroidFacet;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -83,7 +84,7 @@ import org.w3c.dom.Node;
  * Factory of {@link MergedManifestSnapshot}. The created snapshots represent the merged manifest state at a
  * point in time and are immutable.
  */
-class MergedManifestSnapshotFactory {
+public class MergedManifestSnapshotFactory {
 
   /**
    * A resource value defined by the manifest. Unlike its base class, does not need to keep a reference
@@ -150,7 +151,7 @@ class MergedManifestSnapshotFactory {
    * manifest snapshot that indicates that we should not retry with bad manifest before it's changed
    */
   @NotNull
-  static MergedManifestSnapshot createEmptyMergedManifestSnapshot(
+  public static MergedManifestSnapshot createEmptyMergedManifestSnapshot(
     @NotNull Module module,
     @Nullable AndroidFacet facet,
     @Nullable Exception exception
@@ -180,9 +181,12 @@ class MergedManifestSnapshotFactory {
 
 
   @NotNull
-  static MergedManifestSnapshot createMergedManifestSnapshot(@NotNull AndroidFacet facet) {
+  static MergedManifestSnapshot createMergedManifestSnapshot(
+    @NotNull AndroidFacet facet,
+    @NotNull Function<@NotNull Module, @NotNull MergedManifestSnapshot> recursiveSnapshotGetter
+  ) {
     try {
-      MergedManifestInfo mergedManifestInfo = MergedManifestInfo.create(facet);
+      MergedManifestInfo mergedManifestInfo = MergedManifestInfo.create(facet, recursiveSnapshotGetter);
 
       Document document = mergedManifestInfo.getXmlDocument();
       Element root = document == null ? null : document.getDocumentElement();
