1d006d5e70f096a23fac6008658d2968b3858e16
Chris Warrington <cmw@google.com>
2026-02-10 17:10:10 -0800

    Tooling model: capture built-in Kotlin
    Which is useful in Studio for correctly handling adding new projects.
    
    Bug: 476860234
    Test: Updated snapshots
    Change-Id: I65d346fcdb5a7be61a55d98d58c63b1ccdd354ae

--- android-test-framework/testSrc/com/android/tools/idea/testing/AndroidGradleTestUtils.kt
+++ android-test-framework/testSrc/com/android/tools/idea/testing/AndroidGradleTestUtils.kt
@@ -920,6 +920,7 @@ fun AndroidProjectStubBuilder.buildAgpProjectFlagsStub(): IdeAndroidGradlePlugin
     disableAgpUpgradePrompt = false,
     useCustomManagedDevices = false,
     highlightGradualR8Api = false,
+    builtInKotlinDefaultEnabled = true,
   )
 
 fun AndroidProjectStubBuilder.buildDefaultConfigStub() =
