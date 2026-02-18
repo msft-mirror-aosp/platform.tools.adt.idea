86ad957ecb0a3d77f3dbdd7596aabf90112723d5
Ruslan Cheremin <ruslan.cheremin@jetbrains.com>
2026-02-18 11:07:36 +0700

    cleanup [vfs]: incorrect ``@NotNull` annotation fixed
    GitOrigin-RevId: 62b55ef8b64f641a6fc1f138662fda5f3321cba8
    (cherry picked from commit ba99d1b5ea7a8abc90fb1517ffa34528f2ca6c6c)
    Change-Id: I1aeabdcf1e92f8e3f492e67f58a1a7c144d6f2fb

--- designer/src/com/android/tools/idea/editors/layeredimage/EmptyVirtualFile.java
+++ designer/src/com/android/tools/idea/editors/layeredimage/EmptyVirtualFile.java
@@ -50,8 +50,7 @@ final class EmptyVirtualFile extends VirtualFile {
   }
 
   @Override
-  @NotNull
-  public byte[] contentsToByteArray() throws IOException {
+  public byte @NotNull [] contentsToByteArray() throws IOException {
     return EMPTY_ARRAY;
   }
 
