c1dff841641fa418da8c4538b49cb6e0562d89ab
Jinseong Jeon <jsjeon@google.com>
2026-02-18 22:38:24 -0800

    Drop K1 mode usages in ide-perf-tests
    No need to check if it's K2 mode, since K1 mode is not allowed anymore.
    
    Bug: 468001716
    Test: presubmit
    Change-Id: I02a2fd6cccfb4d6929744157ebddab5f54066715

--- ide-perf-tests/testSrc/com/android/tools/idea/benchmarks/SantaTrackerKotlinK2Benchmark.kt
+++ ide-perf-tests/testSrc/com/android/tools/idea/benchmarks/SantaTrackerKotlinK2Benchmark.kt
@@ -20,7 +20,6 @@ import com.intellij.ide.highlighter.JavaFileType
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.openapi.fileTypes.LanguageFileType
 import org.jetbrains.kotlin.idea.KotlinFileType
-import org.jetbrains.kotlin.idea.base.plugin.KotlinPluginModeProvider
 import org.junit.BeforeClass
 import org.junit.ClassRule
 import org.junit.Test
@@ -38,7 +37,6 @@ class SantaTrackerKotlinK2Benchmark : FullProjectBenchmark() {
     @BeforeClass
     fun setUpBeforeClass() {
       loadProject(staticRule, GRADLE_PROJECT_NAME)
-      assert(KotlinPluginModeProvider.isK2Mode())
     }
   }
 
