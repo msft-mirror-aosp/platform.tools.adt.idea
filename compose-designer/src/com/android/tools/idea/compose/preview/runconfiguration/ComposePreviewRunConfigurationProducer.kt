5083c3903fb0a105109df3fe044da6c5ee5a6582
Jinseong Jeon <jsjeon@google.com>
2026-02-26 10:26:51 -0800

    Drop dead code (K1) in compose-ide-plugin
    ComposableFunctionExtractableAnalyser should have been deleted together
    at Id0be9898bd0593412db5746be677e83f15aa1a3c
    when dropping plugin.k1.xml
    
    Usage in ComposePreviewRunConfigurationProducer is dead code
    after Id0c8810c5cc33453f0f573b129d0d383c946a680
    
    Bug: 454618181
    Bug: 468001716
    Test: presubmit
    Change-Id: I8fd375493e1ff0e5440fd0cc42e5850d2934e372

--- compose-designer/src/com/android/tools/idea/compose/preview/runconfiguration/ComposePreviewRunConfigurationProducer.kt
+++ compose-designer/src/com/android/tools/idea/compose/preview/runconfiguration/ComposePreviewRunConfigurationProducer.kt
@@ -36,17 +36,12 @@ import org.jetbrains.kotlin.analysis.api.annotations.KaAnnotationValue
 import org.jetbrains.kotlin.analysis.api.permissions.KaAllowAnalysisOnEdt
 import org.jetbrains.kotlin.analysis.api.permissions.allowAnalysisOnEdt
 import org.jetbrains.kotlin.analysis.api.types.KaClassType
-import org.jetbrains.kotlin.idea.caches.resolve.analyze as analyzeK1
 import org.jetbrains.kotlin.name.ClassId
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.name.Name
-import org.jetbrains.kotlin.psi.KtAnnotationEntry
 import org.jetbrains.kotlin.psi.KtNamedFunction
 import org.jetbrains.kotlin.psi.KtParameter
 import org.jetbrains.kotlin.psi.psiUtil.getNonStrictParentOfType
-import org.jetbrains.kotlin.resolve.BindingContext
-import org.jetbrains.kotlin.resolve.constants.KClassValue
-import org.jetbrains.kotlin.resolve.lazy.BodyResolveMode
 
 /**
  * Producer of [ComposePreviewRunConfiguration] for `@Composable` functions annotated with [PREVIEW_ANNOTATION_FQN]. The configuration
@@ -108,13 +103,6 @@ private fun updateConfigurationTriggerToGutterIfNeeded(configuration: ComposePre
   if (PlatformCoreDataKeys.CONTEXT_COMPONENT.getData(context.dataContext) is EditorGutter) {
     configuration.triggerSource = ComposePreviewRunConfiguration.TriggerSource.GUTTER
   }
-}
-
-/** Get the provider fully qualified class name of a `@PreviewParameter` annotated parameter. */
-private fun KtAnnotationEntry.providerClassName(): String? {
-  val annotationDescriptor = analyzeK1(BodyResolveMode.PARTIAL).get(BindingContext.ANNOTATION, this) ?: return null
-  val argument = annotationDescriptor.allValueArguments.entries.firstOrNull { it.key.asString() == "provider" }?.value ?: return null
-  return (argument.value as? KClassValue.Value.NormalClass)?.classId?.asSingleFqName()?.asString()
 }
 
 /** Get the provider fully qualified class name of a `@PreviewParameter` annotated parameter. */
