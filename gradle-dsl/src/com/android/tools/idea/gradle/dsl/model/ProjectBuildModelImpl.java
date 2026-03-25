fedb39c2ac99adec009da67d82233ff2eb4629dd
Hisham Haque <hhaque@google.com>
2026-03-25 18:28:21 +0000

    [NPW] Add declarative to the new project wizard
    These changes are not final and due to change as Gradle continues to iterate on declarative.
    
    Bug: 490330486
    Test: updated existing tests
    Change-Id: I1df80fbb915e310b81a77f996599dd711c483dda

--- gradle-dsl/src/com/android/tools/idea/gradle/dsl/model/ProjectBuildModelImpl.java
+++ gradle-dsl/src/com/android/tools/idea/gradle/dsl/model/ProjectBuildModelImpl.java
@@ -77,7 +77,17 @@ public class ProjectBuildModelImpl implements ProjectBuildModel {
   @Nullable
   public GradleBuildModel getModuleBuildModel(@NotNull Module module) {
     VirtualFile file = myBuildModelContext.getGradleBuildFile(module);
-    return file == null ? null : getModuleBuildModel(file);
+    if (file == null) {
+      return null;
+    }
+
+    if (DeclarativeStudioSupport.isEnabled()) {
+      GradleDeclarativeBuildModel declarativeModel = getDeclarativeModuleBuildModel(file);
+      if (declarativeModel != null) {
+        return declarativeModel;
+      }
+    }
+    return getModuleBuildModel(file);
   }
 
   @Override
@@ -140,7 +150,6 @@ public class ProjectBuildModelImpl implements ProjectBuildModel {
   @Override
   @Nullable
   public GradleDeclarativeSettingsModel getDeclarativeSettingsModel() {
-    if (!DeclarativeStudioSupport.isEnabled()) return null;
     VirtualFile virtualFile = getProjectSettingsFile();
     if (virtualFile == null) return null;
     if (!virtualFile.getName().equals(FN_SETTINGS_GRADLE_DECLARATIVE)) return null;
