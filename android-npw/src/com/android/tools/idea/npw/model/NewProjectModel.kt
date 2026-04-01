3085922c6730958f8da182ae0c6d8627b67a3b13
Hisham Haque <hhaque@google.com>
2026-04-01 10:19:53 +0000

    Add dslLanguage to projectDataTemplate
    Bug: n/a
    Test: existing
    Change-Id: Ic7b15c76c33bc881c43495957a9810f076a75452

--- android-npw/src/com/android/tools/idea/npw/model/NewProjectModel.kt
+++ android-npw/src/com/android/tools/idea/npw/model/NewProjectModel.kt
@@ -292,6 +292,7 @@ class NewProjectModel : WizardModel(), ProjectModelData {
             language = this@NewProjectModel.language.value
             agpVersion = resolvedAgpVersion
             additionalMavenRepos = this@NewProjectModel.additionalMavenRepos.get()
+            dslLanguage = this@NewProjectModel.dslLanguage.get()
           }
           .build()
     }
@@ -329,12 +330,7 @@ class NewProjectModel : WizardModel(), ProjectModelData {
         RenderingContext(project, null, "New Project", projectTemplateData, showErrors = true, dryRun = dryRun, moduleRoot = null)
       val executor = if (dryRun) FindReferencesRecipeExecutor(context) else DefaultRecipeExecutor(context)
       val recipe: Recipe = { data: TemplateData ->
-        androidProjectRecipe(
-          data = data as ProjectTemplateData,
-          appTitle = applicationName.get(),
-          language = language.value,
-          dslLanguage = dslLanguage.get(),
-        )
+        androidProjectRecipe(data = data as ProjectTemplateData, appTitle = applicationName.get(), language = language.value)
       }
 
       recipe.render(context, executor, AndroidStudioEvent.TemplateRenderer.ANDROID_PROJECT)
--- android-npw/testSrc/com/android/tools/idea/npw/model/NewProjectTemplateRendererTest.kt
+++ android-npw/testSrc/com/android/tools/idea/npw/model/NewProjectTemplateRendererTest.kt
@@ -158,7 +158,7 @@ class NewProjectTemplateRendererTest {
     val newProjectModel = spy(NewProjectModel())
     val render = spy(newProjectModel.ProjectTemplateRenderer())
     val projectTemplateDataBuilder = spy(newProjectModel.projectTemplateDataBuilder)
-    val projectTemplateData = createSimpleProjectTemplateData(gradleVersion)
+    val projectTemplateData = createSimpleProjectTemplateData(gradleVersion, dslLanguage)
 
     newProjectModel.project = projectRule.project
     doReturn(projectTemplateData).whenever(projectTemplateDataBuilder).build()
@@ -180,7 +180,7 @@ class NewProjectTemplateRendererTest {
     return render
   }
 
-  private fun createSimpleProjectTemplateData(gradleVersion: GradleVersion) =
+  private fun createSimpleProjectTemplateData(gradleVersion: GradleVersion, dslLanguage: DslLanguage): ProjectTemplateData =
     ProjectTemplateData(
       false,
       AgpVersions.newProject,
@@ -195,6 +195,7 @@ class NewProjectTemplateRendererTest {
       null,
       null,
       true,
+      dslLanguage = dslLanguage,
     )
 
   private fun assertFoojayPlugin(isApplied: Boolean) {
