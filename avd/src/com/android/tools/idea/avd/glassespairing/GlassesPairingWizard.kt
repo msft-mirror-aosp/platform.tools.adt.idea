3faf58473ef20494539247902142cd8251b30810
Sergey Prigogin <sprigogin@google.com>
2026-04-22 10:52:04 -0700

    Revert "Issue heads-up for phone proactively"
    Reason for revert: showing phone screen too early may create an impression that the user is expected to click on something there.
    
    This reverts commit 5e2205c404cbf6ccb59c8ec1a57db1acab0b54ec.
    
    Bug: Bug: 490500859
    Test: existing
    Change-Id: I388a3ef2083954246697d0c649a6713faec8606c

--- avd/src/com/android/tools/idea/avd/glassespairing/GlassesPairingWizard.kt
+++ avd/src/com/android/tools/idea/avd/glassespairing/GlassesPairingWizard.kt
@@ -90,7 +90,6 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.TimeoutCancellationException
 import kotlinx.coroutines.cancel
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.FlowCollector
@@ -155,7 +154,7 @@ internal interface GlassesPairer {
 
 internal object DefaultGlassesPairer : GlassesPairer {
   override fun pair(glasses: DeviceHandle, phone: DeviceHandle, project: Project?, onMacRetrieved: (String) -> Unit): Flow<PairingState> {
-    return pairGlassesToPhone(glasses, phone, project, onMacRetrieved = onMacRetrieved)
+    return pairGlassesToPhone(glasses, phone, onMacRetrieved = onMacRetrieved)
   }
 }
 
@@ -587,7 +586,6 @@ internal fun launchGlassesAndPhone(glasses: DeviceHandle, phone: DeviceHandle): 
 internal fun pairGlassesToPhone(
   glasses: DeviceHandle,
   phone: DeviceHandle,
-  project: Project?,
   launchFlow: () -> Flow<PairingState> = { launchGlassesAndPhone(glasses, phone) },
   onMacRetrieved: (String) -> Unit,
 ): Flow<PairingState> {
@@ -624,7 +622,6 @@ internal fun pairGlassesToPhone(
       }
 
       try {
-        project?.userInvolvementRequired(phone)
         runPairingSequence(phoneDevice, glassesDevice, phoneName, glassesName, logger, onMacRetrieved)
       } catch (cause: ShellCommandException) {
         GlassesPairingUsageTracker.log(GlassesPairingEvent.EventKind.PAIRING_ERROR_SHELL_COMMAND)
