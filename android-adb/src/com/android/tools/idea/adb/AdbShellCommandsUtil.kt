290b3b7dddc45f42ac1c66a43ba37dd16ea7fa3c
Alexander Demurov <ademurov@google.com>
2026-04-28 17:49:47 +0000

    Remove unused code from device explorer AdbShellCommandsUtil
    Bug: not filed
    Test: existing
    Change-Id: Icc408e002d3255b69411ae5d7123d9a20d1d1cfd

--- android-adb/src/com/android/tools/idea/adb/AdbShellCommandsUtil.kt
+++ android-adb/src/com/android/tools/idea/adb/AdbShellCommandsUtil.kt
@@ -18,28 +18,22 @@ package com.android.tools.idea.adb
 import com.android.adblib.ConnectedDevice
 import com.android.adblib.ShellCollector
 import com.android.adblib.TextShellCollector
-import com.android.adblib.ddmlibcompatibility.debugging.executeShellCommand
 import com.android.adblib.selector
 import com.android.ddmlib.DdmPreferences
-import com.android.ddmlib.IDevice
-import com.android.tools.idea.adblib.AdbLibApplicationService
 import com.google.common.base.Stopwatch
 import com.intellij.openapi.diagnostic.thisLogger
 import java.time.Duration
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.single
 
-abstract class AdbShellCommandsUtil {
+class AdbShellCommandsUtil(private val device: ConnectedDevice) {
   private val logger = thisLogger()
 
+  /** Executes an ADB shell command, providing error-detection support for legacy devices that do not support the Shell v2 protocol. */
   suspend fun executeCommand(command: String): AdbShellCommandResult {
-    return executeCommandImpl(command, true)
-  }
-
-  private suspend fun executeCommandImpl(command: String, errorCheck: Boolean): AdbShellCommandResult {
     // Adding the " || echo xxx" command to the command allows us to detect non-zero status code
-    // from the command by analysing the output and looking for the "xxx" marker.
-    val fullCommand = if (errorCheck) command + COMMAND_ERROR_CHECK_SUFFIX else command
+    // from the command by analyzing the output and looking for the "xxx" marker.
+    val fullCommand = command + COMMAND_ERROR_CHECK_SUFFIX
     val commandOutput: MutableList<String> = ArrayList()
     val stopwatch = Stopwatch.createStarted()
     val receiver = TextShellCollector()
@@ -49,10 +43,7 @@ abstract class AdbShellCommandsUtil {
     // Look for error marker in the last 2 output lines
     var isError = false
     if (
-      errorCheck &&
-        commandOutput.size >= 2 &&
-        commandOutput[commandOutput.size - 2] == ERROR_LINE_MARKER &&
-        commandOutput[commandOutput.size - 1] == ""
+      commandOutput.size >= 2 && commandOutput[commandOutput.size - 2] == ERROR_LINE_MARKER && commandOutput[commandOutput.size - 1] == ""
     ) {
       isError = true
       commandOutput.removeLast()
@@ -75,29 +66,22 @@ abstract class AdbShellCommandsUtil {
     return AdbShellCommandResult(command, commandOutput, isError)
   }
 
-  abstract fun <T> executeCommandImpl(command: String, receiver: ShellCollector<T>): Flow<T>
+  private fun <T> executeCommandImpl(command: String, receiver: ShellCollector<T>): Flow<T> {
+    return device.session.deviceServices.shell(
+      device.selector,
+      command,
+      receiver,
+      commandTimeout = Duration.ofMillis(DdmPreferences.getTimeOut().toLong()),
+    )
+  }
 
   companion object {
     private const val ERROR_LINE_MARKER = "ERR-ERR-ERR-ERR"
     private const val COMMAND_ERROR_CHECK_SUFFIX = " || echo $ERROR_LINE_MARKER"
 
     @JvmStatic
-    fun create(device: IDevice) =
-      object : AdbShellCommandsUtil() {
-        override fun <T> executeCommandImpl(command: String, receiver: ShellCollector<T>): Flow<T> =
-          executeShellCommand(AdbLibApplicationService.instance.session, device, command, receiver)
-      }
-
-    @JvmStatic
-    fun create(device: ConnectedDevice) =
-      object : AdbShellCommandsUtil() {
-        override fun <T> executeCommandImpl(command: String, receiver: ShellCollector<T>): Flow<T> =
-          device.session.deviceServices.shell(
-            device.selector,
-            command,
-            receiver,
-            commandTimeout = Duration.ofMillis(DdmPreferences.getTimeOut().toLong()),
-          )
-      }
+    fun create(device: ConnectedDevice): AdbShellCommandsUtil {
+      return AdbShellCommandsUtil(device)
+    }
   }
 }
