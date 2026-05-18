2b7e66f84620e5c1e45ce64c4716091dd5f9904a
Mike Moroney <mmoro@google.com>
2026-05-18 11:14:52 -0700

    Create diagnostics report on background thread
    Bug: 486995469, 490020053
    Test: ZipUtilTest
    Change-Id: I92f51d6e66f2b370bed8ecb23d14fcf033f668dc

--- android/src/com/android/tools/idea/util/ZipUtil.kt
+++ android/src/com/android/tools/idea/util/ZipUtil.kt
@@ -15,25 +15,76 @@
  */
 package com.android.tools.idea.util
 
+import com.intellij.openapi.diagnostic.logger
+import com.intellij.openapi.progress.ProgressIndicator
+import com.intellij.openapi.progress.Task
+import com.intellij.openapi.project.Project
 import java.io.FileOutputStream
 import java.io.IOException
 import java.nio.file.Files
+import java.nio.file.Path
 import java.nio.file.Paths
 import java.util.zip.ZipEntry
 import java.util.zip.ZipOutputStream
 
+/**
+ * Represents a file to be included in the zip archive.
+ *
+ * @property path The full system path to the source file.
+ * @property name The desired name of the file within the zip archive.
+ */
 data class ZipData(val path: String, val name: String)
 
-fun zipFiles(files: Array<ZipData>, destination: String) {
+private val LOG = logger<ZipData>()
+
+/**
+ * Compresses the given files into a zip archive at the specified destination.
+ *
+ * @param files An array of [ZipData] objects, each representing a file to include in the zip.
+ * @param destination The full system path where the resulting zip file will be created.
+ * @param progressCallback An optional callback function invoked for each file after it's added to the zip, receiving the [ZipData] of the
+ *   file. This can be used to report progress.
+ */
+fun zipFiles(files: List<ZipData>, destination: String, progressCallback: ((ZipData) -> Unit)? = null) {
   FileOutputStream(destination).use { output ->
     ZipOutputStream(output).use { zip ->
       for (zipData in files) {
-        zip.putNextEntry(ZipEntry(zipData.name))
         try {
+          zip.putNextEntry(ZipEntry(zipData.name))
           Files.copy(Paths.get(zipData.path), zip)
-        } catch (_: IOException) {}
-        zip.closeEntry()
+        } catch (e: IOException) {
+          LOG.warn("Failed to add file to zip: ${zipData.path}", e)
+        } finally {
+          zip.closeEntry()
+        }
+        progressCallback?.invoke(zipData)
       }
     }
+  }
+}
+
+/**
+ * A background task for compressing files into a zip archive, with progress indication.
+ *
+ * @param project The project associated with this task.
+ * @param title The title of the background task visible to the user.
+ * @property zipData A list of [ZipData] objects to be compressed.
+ * @property path The destination [Path] for the output zip file.
+ */
+open class CompressFilesTask(project: Project?, title: String, val zipData: List<ZipData>, val path: Path) :
+  Task.Backgroundable(project, title, true) {
+  override fun run(indicator: ProgressIndicator) {
+    indicator.fraction = 0.0
+    indicator.isIndeterminate = false
+
+    var processed = 0
+    zipFiles(zipData, path.toString()) { currentFile ->
+      indicator.checkCanceled()
+      processed++
+      indicator.fraction = processed.toDouble() / zipData.size
+      indicator.text2 = "Compressing: ${currentFile.name}"
+    }
+
+    indicator.text = "Finished compressing files."
   }
 }
