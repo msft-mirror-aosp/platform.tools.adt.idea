09392a89d53e1b8de9830086a92fff95680325a2
Sharanya Saha <sharanyasaha@google.com>
2026-06-16 13:41:51 +0000

    Remove dead helper code for J/K Method Sampling integration tests
    This change removes dead helper methods used by Java/Kotlin
    Method recording integration tests, as the tests have been updated
    to use the Tracing recording type instead of Sampling.
    
    Bug: 504847424
    Test: N/A
    Change-Id: I79e2ff207790d91bc878b54254f3383fbc7a6a02

--- profilers-integration/testSrc/com/android/tools/profilers/integration/ProfilersTestBase.kt
+++ profilers-integration/testSrc/com/android/tools/profilers/integration/ProfilersTestBase.kt
@@ -271,10 +271,6 @@ open class ProfilersTestBase {
     studio.executeAction("Android.StopJavaKotlinAllocationsTask")
   }
 
-  protected fun setRecordingTypeToSampling(studio: AndroidStudio) {
-    studio.executeAction("Android.SetRecordingTypeToSampling")
-  }
-
   protected fun setRecordingTypeToTracing(studio: AndroidStudio) {
     studio.executeAction("Android.SetRecordingTypeToTracing")
   }
