0c830aa8929eade43a4ae62aa5e4af943af8fc0b
Jacob Logsdon <jlogs@google.com>
2026-06-23 19:35:10 +0000

    Remove StudioRenderSecurity
    It caused runtime exceptions with JBR 25 enabled, so this code can no
    longer be active.
    
    Bug: 505820844
    Change-Id: I2c0c5580271853752a29b8eee7e52edd6b6c502a

--- android/src/org/jetbrains/android/uipreview/StudioRenderSecurity.kt
+++ android/src/org/jetbrains/android/uipreview/StudioRenderSecurity.kt
@@ -16,54 +16,36 @@
 package org.jetbrains.android.uipreview
 
 import com.android.tools.idea.flags.StudioFlags
-import com.android.tools.idea.log.LogWrapper
-import com.android.tools.idea.rendering.StudioRenderSecurityManager
 import com.android.tools.rendering.security.AllowAllRenderSandbox
 import com.android.tools.rendering.security.PreCheckRenderSandboxDelegate
 import com.android.tools.rendering.security.RenderSandbox
 import com.android.tools.rendering.security.RenderSecurity
 import com.android.tools.rendering.security.RenderSecurityManager
-import com.intellij.openapi.diagnostic.Logger
 
 /**
- * Android Studio specific implementation of [RenderSecurity] that supports both [StudioRenderSecurityManager] and [StudioRenderSandbox].
+ * Android Studio specific implementation of [RenderSecurity] that supports [RenderSandbox].
  */
 @Suppress("VisibleForTests")
 class StudioRenderSecurity(val sdkPath: String?, val projectPath: String?, val appTempDir: String?) : RenderSecurity {
 
-  private val securityManager: StudioRenderSecurityManager?
   private val sandbox: RenderSandbox?
   private val useSandbox = StudioFlags.RENDER_SANDBOX.get()
   private var previousSandbox: RenderSandbox? = null
 
   init {
-    securityManager = StudioRenderSecurityManager(sdkPath, projectPath, false)
-    securityManager.setLogger(
-      LogWrapper(Logger.getInstance(StudioRenderSecurityManager::class.java)).alwaysLogAsDebug(true).allowVerbose(false)
-    )
-    securityManager.setAppTempDir(appTempDir)
-
     val baseSandbox = StudioRenderSandbox(sdkPath, projectPath, appTempDir)
     sandbox = PreCheckRenderSandboxDelegate(baseSandbox, { RenderSecurityManager.sEnabled })
   }
 
   override fun activate(credential: Any) {
-    if (useSandbox) {
-      if (sandbox != null) {
-        previousSandbox = RenderSandbox.setRenderSandbox(sandbox)
-      }
-    } else {
-      securityManager?.setActive(true, credential)
+    if (useSandbox && sandbox != null) {
+      previousSandbox = RenderSandbox.setRenderSandbox(sandbox)
     }
   }
 
   override fun deactivate(credential: Any) {
-    if (useSandbox) {
-      if (sandbox != null) {
-        RenderSandbox.setRenderSandbox(previousSandbox ?: AllowAllRenderSandbox)
-      }
-    } else {
-      securityManager?.dispose(credential)
+    if (useSandbox && sandbox != null) {
+      RenderSandbox.setRenderSandbox(previousSandbox ?: AllowAllRenderSandbox)
     }
   }
 }
