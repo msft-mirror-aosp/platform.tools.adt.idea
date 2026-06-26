4eca581a28a257cdd6f9b2c9fcd8c27393fc7788
Sergey Prigogin <sprigogin@google.com>
2026-06-26 16:55:55 -0700

    Revert "Simplify synchronization in overrideRegistrationDirectory"
    This reverts commit 870a08488a2753481cb710184919c60672d90e79.
    
    Reason for revert: introduced test flakiness due to coroutine scheduling in a test environment
    
    Bug: N/A
    Change-Id: I9d050d37286e02f70c2b75a22b67eda9c4c57ea7

--- streaming/src/com/android/tools/idea/streaming/emulator/RunningEmulatorCatalog.kt
+++ streaming/src/com/android/tools/idea/streaming/emulator/RunningEmulatorCatalog.kt
@@ -49,19 +49,20 @@ import java.nio.file.StandardWatchEventKinds.OVERFLOW
 import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
+import java.util.concurrent.locks.ReentrantReadWriteLock
 import java.util.regex.Pattern
+import kotlin.concurrent.read
+import kotlin.concurrent.write
 import kotlin.io.path.deleteIfExists
 import kotlin.time.Duration.Companion.milliseconds
 import kotlinx.coroutines.CompletableDeferred
 import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import org.jetbrains.annotations.TestOnly
 
 /** Keeps track of Android Emulators running on the local machine under the current user account. */
@@ -72,6 +73,8 @@ class RunningEmulatorCatalog : Disposable.Parent {
   private val fileNamePattern = Pattern.compile("pid_(\\d+).ini")
   @GuardedBy("dataLock") private var updateJob: Job? = null
   @Volatile private var isDisposing = false
+  /** This lock is held for reading while an update is running. */
+  private val updateLock = ReentrantReadWriteLock()
   private val updateChannel = Channel<Unit>(Channel.CONFLATED)
   @GuardedBy("dataLock") private var updateWorkerJob: Job? = null
   private val dataLock = Any()
@@ -178,7 +181,7 @@ class RunningEmulatorCatalog : Disposable.Parent {
       updateWorkerJob =
         scope.launch(Dispatchers.IO) {
           for (request in updateChannel) {
-            update()
+            updateLock.read { update() }
           }
         }
     }
@@ -523,8 +526,7 @@ class RunningEmulatorCatalog : Disposable.Parent {
       registrationDirectory = directory ?: computeRegistrationDirectory()
     }
 
-    runBlocking { updateWorkerJob?.cancelAndJoin() }
-    startUpdateWorker()
+    updateLock.write {} // Make sure that previously running updates have finished.
     startWatchService()
   }
 
