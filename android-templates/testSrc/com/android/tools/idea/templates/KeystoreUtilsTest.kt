4cb4159290e0b4bddc79d6cbe2019f3f60aa4463
[BKunal Dagar <kunaldagar@google.com>
2026-06-27 10:27:58 +0000

    Harden KeystoreUtils against untrusted projects
    Gate getDebugKeystore on project trust. Fall back to the default debug keystore if the project is not trusted .
    
    Bug: 514206807
    Test: KeystoreUtilsTest
    
    Change-Id: Ic328ff1b92f5c13273d27216a8ee25539e34d9c9

--- android-templates/testSrc/com/android/tools/idea/templates/KeystoreUtilsTest.kt
+++ android-templates/testSrc/com/android/tools/idea/templates/KeystoreUtilsTest.kt
@@ -15,11 +15,21 @@
  */
 package com.android.tools.idea.templates
 
+import com.android.ide.common.signing.KeystoreHelper
+import com.android.prefs.AndroidLocationsSingleton
+import com.android.tools.idea.testing.AndroidProjectRule
+import com.intellij.ide.trustedProjects.TrustedProjects
 import java.io.File
-import junit.framework.TestCase
 import org.jetbrains.android.AndroidTestCase
+import org.jetbrains.android.facet.AndroidFacet
+import org.junit.Assert.assertEquals
+import org.junit.Assert.fail
+import org.junit.Rule
+import org.junit.Test
 
-class KeystoreUtilsTest : TestCase() {
+class KeystoreUtilsTest {
+  @get:Rule val projectRule = AndroidProjectRule.inMemory().initAndroid(true)
+
   private fun check(certPath: String, expected: String) {
     try {
       assertEquals(expected, KeystoreUtils.sha1(File(certPath)))
@@ -28,8 +38,36 @@ class KeystoreUtilsTest : TestCase() {
     }
   }
 
+  @Test
   fun testSha1() {
     val certPath = AndroidTestCase.getTestDataPath() + File.separator + "signingKey" + File.separator + "debug.keystore"
     check(certPath, "6B:D1:08:20:E4:95:86:82:19:3C:36:D8:C2:C9:52:CB:A8:19:1A:54")
+  }
+
+  @Test
+  fun testGetDebugKeystore_trusted() {
+    val facet = AndroidFacet.getInstance(projectRule.module)!!
+    val state = facet.configuration.state
+    val customPath = "${projectRule.fixture.tempDirPath}/debug.keystore"
+    state.CUSTOM_DEBUG_KEYSTORE_PATH = customPath
+
+    TrustedProjects.setProjectTrusted(projectRule.project, true)
+
+    val keystoreFile = KeystoreUtils.getDebugKeystore(facet)
+    assertEquals(File(customPath), keystoreFile)
+  }
+
+  @Test
+  fun testGetDebugKeystore_untrusted() {
+    val facet = AndroidFacet.getInstance(projectRule.module)!!
+    val state = facet.configuration.state
+    val customPath = "${projectRule.fixture.tempDirPath}/debug.keystore"
+    state.CUSTOM_DEBUG_KEYSTORE_PATH = customPath
+
+    TrustedProjects.setProjectTrusted(projectRule.project, false)
+
+    val keystoreFile = KeystoreUtils.getDebugKeystore(facet)
+    val defaultKeystoreFile = KeystoreHelper.defaultDebugKeystoreLocation(AndroidLocationsSingleton)
+    assertEquals(defaultKeystoreFile, keystoreFile)
   }
 }
