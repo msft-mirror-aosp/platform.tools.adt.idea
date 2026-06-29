53dbd1d784d2204d0ecfa08fc8b4fd3b1a43d597
Sergey Prigogin <sprigogin@google.com>
2026-06-29 12:48:29 -0700

    Workaround for b/528439369
    Test: existing
    Bug: 528439369
    Change-Id: Ic6cac6ac524c8d894a5afc1d2a59fcc3252f95e7

--- streaming/src/com/android/tools/idea/streaming/emulator/actions/EnvironmentTracker.kt
+++ streaming/src/com/android/tools/idea/streaming/emulator/actions/EnvironmentTracker.kt
@@ -37,7 +37,9 @@ internal class EnvironmentTracker(private val emulator: EmulatorController) {
       val observer =
         object : EmptyStreamObserver<Environment>() {
           override fun onNext(message: Environment) {
-            _environment = message
+            if (!message.environmentMap.isEmpty()) { // TODO: Don't ignore empty environment when b/528439369 is fixed.
+              _environment = message
+            }
             latch.countDown()
           }
 
