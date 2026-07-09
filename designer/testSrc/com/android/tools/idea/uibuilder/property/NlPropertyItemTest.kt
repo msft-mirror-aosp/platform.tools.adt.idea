3e9fc027c197f1eba6e196cfdecb03e1aa2b1a57
Amaury Medeiros <amaurym@google.com>
2026-07-09 12:05:01 +0000

    Re-enable NlPropertyItemTest#testLazyResolverLoading
    This test was fixed by change I6b25f04f but not actually re-enabled.
    
    Bug: 356584506
    Bug: 532077542
    Test: NlPropertyItemTest#testLazyResolverLoading
    Change-Id: Ie160348d6e60cd22bbbb52e34c1d960f23fef0c2

--- designer/testSrc/com/android/tools/idea/uibuilder/property/NlPropertyItemTest.kt
+++ designer/testSrc/com/android/tools/idea/uibuilder/property/NlPropertyItemTest.kt
@@ -755,7 +755,6 @@ class NlPropertyItemTest {
     assertThat(property.value).isEqualTo("@string/demo")
   }
 
-  @Ignore("b/356584506")
   @Test
   fun testLazyResolverLoading() =
     testScope.runTest {
