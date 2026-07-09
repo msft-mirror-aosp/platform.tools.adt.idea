b4818bc0ab8af074996b69aefab18cb1348c9bd0
Matthew Gharrity <gharrma@google.com>
2026-07-08 21:41:03 -0700

    PsiCallParameterPropertyItem: migrate from deprecated K1 API
    This is for compatibility with IntelliJ 2026.2.
    
    I looked at a few examples in IntelliJ sources to find the idiomatic
    solution. Fortunately, this code path is very well covered by tests.
    
    Bug: 499067701
    Test: PreviewPickerTests (it has good coverage)
    Change-Id: If56c1a9ed88c5799d14c0b7082d03b94fafcd8c9

--- compose-designer/src/com/android/tools/idea/compose/pickers/base/property/PsiCallParameterPropertyItem.kt
+++ compose-designer/src/com/android/tools/idea/compose/pickers/base/property/PsiCallParameterPropertyItem.kt
@@ -34,9 +34,7 @@ import com.intellij.psi.codeStyle.CodeStyleManager
 import com.intellij.util.concurrency.AppExecutorUtil
 import com.intellij.util.text.nullize
 import java.util.concurrent.Callable
-import org.jetbrains.kotlin.K1Deprecation
 import org.jetbrains.kotlin.analysis.api.analyze
-import org.jetbrains.kotlin.idea.core.deleteElementAndCleanParent
 import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.psi.KtConstantExpression
 import org.jetbrains.kotlin.psi.KtExpression
@@ -44,6 +42,7 @@ import org.jetbrains.kotlin.psi.KtLiteralStringTemplateEntry
 import org.jetbrains.kotlin.psi.KtPsiFactory
 import org.jetbrains.kotlin.psi.KtStringTemplateExpression
 import org.jetbrains.kotlin.psi.KtValueArgument
+import org.jetbrains.kotlin.psi.KtValueArgumentList
 
 private val WRITE_COMMAND = { parameterName: String -> "Parameter $parameterName Modification" }
 private const val DELETE_COMMAND = "Delete Parameter"
@@ -170,10 +169,19 @@ internal open class PsiCallParameterPropertyItem(
   }
 
   @UiThread
-  @OptIn(K1Deprecation::class)
   fun deleteParameter() {
     runModification(DELETE_COMMAND) {
-      argumentExpression?.parent?.deleteElementAndCleanParent()
+      val arg = argumentExpression?.parent
+      if (arg is KtValueArgument) {
+        val argList = arg.parent
+        if (argList is KtValueArgumentList) {
+          if (argList.arguments.size == 1) {
+            argList.delete() // This deletes the parentheses too, which are unnecessary on annotations with no args.
+          } else {
+            argList.removeArgument(arg)
+          }
+        }
+      }
       argumentExpression = null
     }
   }
