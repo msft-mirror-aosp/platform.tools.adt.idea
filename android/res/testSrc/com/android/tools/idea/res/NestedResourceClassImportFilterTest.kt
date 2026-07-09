bfc3cf7a4ba28d39615f66ec33295a55f635e562
Matthew Gharrity <gharrma@google.com>
2026-07-09 08:23:03 -0700

    NestedResourceClassImportFilter: remove K1 vestiges
    In IntelliJ 2026.2 JetBrains made ClassImportFilter work with K2:
    https://youtrack.jetbrains.com/issue/KTIJ-37810.
    
    Note that innerResourceClassNotImported_inlineMethod() still fails but
    for a different reason (b/536060047) so we keep it ignored.
    
    Bug: 499067701
    Bug: 388149042
    Test: NestedResourceClassImportFilterTest
    Change-Id: I6fb8a26ac0a313a2a2510813144856136763b38f

--- android/res/testSrc/com/android/tools/idea/res/NestedResourceClassImportFilterTest.kt
+++ android/res/testSrc/com/android/tools/idea/res/NestedResourceClassImportFilterTest.kt
@@ -24,7 +24,6 @@ import com.google.common.truth.Truth.assertThat
 import com.intellij.application.options.CodeStyle
 import com.intellij.testFramework.RunsInEdt
 import kotlin.test.fail
-import org.jetbrains.kotlin.K1Deprecation
 import org.jetbrains.kotlin.descriptors.ClassKind
 import org.jetbrains.kotlin.descriptors.Modality
 import org.jetbrains.kotlin.descriptors.Visibility
@@ -50,7 +49,6 @@ class NestedResourceClassImportFilterTest {
   /** Tests the actual behavior in the editor as specified by b/254492800. */
   @Test
   @RunsInEdt
-  @Ignore("b/388149042: no counterpart of ClassImportFilter")
   fun innerResourceClassNotImported_otherImport() {
     CodeStyle.getSettings(project).kotlinCustomSettings.IMPORT_NESTED_CLASSES = true
     configureStringResources()
@@ -99,7 +97,7 @@ class NestedResourceClassImportFilterTest {
   /** Tests the actual behavior in the editor as specified by b/254502853. */
   @Test
   @RunsInEdt
-  @Ignore("b/388149042: no counterpart of ClassImportFilter")
+  @Ignore("b/536060047")
   fun innerResourceClassNotImported_inlineMethod() {
     CodeStyle.getSettings(project).kotlinCustomSettings.IMPORT_NESTED_CLASSES = true
     configureStringResources()
@@ -135,7 +133,6 @@ class NestedResourceClassImportFilterTest {
   }
 
   /** More of a unit test for the very specific behavior of the filter. */
-  @OptIn(K1Deprecation::class)
   @Test
   fun allowImport() {
     val ktFile: KtFile = mock()
