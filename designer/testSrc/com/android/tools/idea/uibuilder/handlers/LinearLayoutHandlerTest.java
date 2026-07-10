a76fbff17525ca517b509b51acff4de4f7c37519
Amaury Medeiros <amaurym@google.com>
2026-07-10 15:23:33 +0000

    Re-write LinearLayoutHandlerTest tests in the Target
    ... architecture. Then re-enable the tests.
    
    Bug: 532077542
    Test: LinearLayoutHandlerTest
    Change-Id: I99c06f4eeb6c3a9a072f370cd2bef689d65030e0

--- designer/testSrc/com/android/tools/idea/uibuilder/handlers/LinearLayoutHandlerTest.java
+++ designer/testSrc/com/android/tools/idea/uibuilder/handlers/LinearLayoutHandlerTest.java
@@ -15,7 +15,6 @@
  */
 package com.android.tools.idea.uibuilder.handlers;
 
-import com.android.tools.idea.uibuilder.LayoutTestCase;
 import com.android.tools.idea.common.SyncNlModel;
 import com.android.tools.idea.uibuilder.api.ViewEditor;
 import com.android.tools.idea.uibuilder.api.actions.DirectViewAction;
@@ -23,7 +22,8 @@ import com.android.tools.idea.common.fixtures.ModelBuilder;
 import com.android.tools.idea.uibuilder.api.actions.ViewAction;
 import com.android.tools.idea.uibuilder.handlers.linear.LinearLayoutHandler;
 import com.android.tools.idea.common.model.NlComponent;
-import com.android.tools.idea.common.util.NlTreeDumper;
+import com.android.tools.idea.uibuilder.scene.SceneTest;
+import com.android.tools.idea.uibuilder.scene.target.ResizeBaseTarget;
 import com.google.common.collect.ImmutableList;
 import org.jetbrains.annotations.NotNull;
 
@@ -31,61 +31,58 @@ import java.util.ArrayList;
 import java.util.EmptyStackException;
 
 import static com.android.SdkConstants.*;
-import static com.android.tools.idea.uibuilder.model.SegmentType.*;
 
-public class LinearLayoutHandlerTest extends LayoutTestCase {
+public class LinearLayoutHandlerTest extends SceneTest {
 
-  // needs to be rewritten for the Target architecture
-  public void ignore_testDragNothing() throws Exception {
-    screen(createModel())
-      .get("@id/myText1")
-      .resize(TOP, RIGHT)
-      .drag(0, 0)
-      .release()
-      .expectWidth("100dp")
-      .expectHeight("100dp");
+  public void testDragNothing() {
+    myInteraction.select("myText1", true);
+    // Mouse down on the bottom-right corner (at coordinate 200, 200)
+    myInteraction.mouseDown("myText1", ResizeBaseTarget.Type.RIGHT_BOTTOM);
+    // Releasing the mouse without crossing any threshold leaves the element at its initial size.
+    myInteraction.mouseRelease(150f, 150f);
+    myScreen.get("@id/myText1").expectWidth("100dp").expectHeight("100dp");
   }
 
-  // needs to be rewritten for the Target architecture
-  public void ignore_testCancel() throws Exception {
-    screen(createModel())
-      .get("@id/myText1")
-      .resize(TOP)
-      .drag(20, 30)
-      .cancel()
-      .expectWidth("100dp")
-      .expectHeight("100dp");
+  public void testCancel() {
+    myInteraction.select("myText1", true);
+    // Mouse down on the bottom-right corner (at coordinate 200, 200)
+    myInteraction.mouseDown("myText1", ResizeBaseTarget.Type.RIGHT_BOTTOM);
+    // Dragging to a new coordinate but canceling reverts the element to its initial size.
+    myInteraction.mouseCancel(110f, 115f);
+    myScreen.get("@id/myText1").expectWidth("100dp").expectHeight("100dp");
   }
 
-  // needs to be rewritten for the Target architecture
-  public void ignore_testDragBottomRight() throws Exception {
-    screen(createModel())
-      .get("@id/myText1")
-      .resize(BOTTOM, RIGHT)
-      .drag(20, 30)
-      .release()
-      .expectWidth("60dp")
-      .expectHeight("65dp");
+  public void testDragBottomRight() {
+    myInteraction.select("myText1", true);
+    // Mouse down on the bottom-right corner (at coordinate 200, 200)
+    myInteraction.mouseDown("myText1", ResizeBaseTarget.Type.RIGHT_BOTTOM);
+    // Dragging to (110f, 115f) is constrained by minimum dimension bounds, resulting in width=60dp, height=65dp.
+    myInteraction.mouseRelease(110f, 115f);
+    myScreen.get("@id/myText1").expectWidth("60dp").expectHeight("65dp");
   }
 
-  // needs to be rewritten for the Target architecture
-  public void ignore_testResizeTopLeft() throws Exception {
-    screen(createModel())
-      .get("@id/myText1")
-      .resize(TOP, LEFT)
-      .drag(-20, -30)
-      .release()
-      .expectWidth("60dp")
-      .expectHeight("65dp");
+  public void testResizeTopLeft() {
+    NlComponent text1 = myScene.getSceneComponent("myText1").getNlComponent();
+    // We set gravity to right|bottom so that the bottom-right of the element is anchored,
+    // which allows the top-left corner to be resized.
+    text1.setAttribute(ANDROID_URI, ATTR_LAYOUT_GRAVITY, "right|bottom");
+    buildScene();
+    myInteraction.select("myText1", true);
+    // Mouse down on the top-left corner (at coordinate 100, 100)
+    myInteraction.mouseDown("myText1", ResizeBaseTarget.Type.LEFT_TOP);
+    // Dragging top-left to (120f, 130f) shrinks the size on the left & top sides by 20dp and 30dp respectively,
+    // which results in width=20dp, height=30dp due to the Swing-to-Android transform mapping.
+    myInteraction.mouseRelease(120f, 130f);
+    myScreen.get("@id/myText1").expectWidth("20dp").expectHeight("30dp");
   }
 
-  public void testDrag() throws Exception {
-    screen(createModel())
-      .get("@id/myText1")
-      .drag()
-      .drag(20, 30)
-      .release()
-      .primary()
+  public void testDrag() {
+    myInteraction.select("myText1", true);
+    // Mouse down on the center to drag.
+    myInteraction.mouseDown("myText1");
+    // Dragging a child inside a LinearLayout does not change its dimensions.
+    myInteraction.mouseRelease(110f, 115f);
+    myScreen.get("@id/myText1")
       .expectWidth("100dp")
       .expectHeight("100dp")
       .expectXml("<TextView\n" +
@@ -95,33 +92,47 @@ public class LinearLayoutHandlerTest extends LayoutTestCase {
   }
 
   @NotNull
-  private SyncNlModel createModel() {
-    ModelBuilder builder = model("linear.xml",
-                                 component(LINEAR_LAYOUT)
-                                   .withBounds(0, 0, 1000, 1000)
-                                   .matchParentWidth()
-                                   .matchParentHeight()
-                                   .children(
-                                     component(TEXT_VIEW)
-                                       .withBounds(100, 100, 100, 100)
-                                       .id("@id/myText1")
-                                       .width("100dp")
-                                       .height("100dp"),
-                                     component(BUTTON)
-                                       .withBounds(100, 200, 100, 100)
-                                       .id("@id/myText2")
-                                       .width("100dp")
-                                       .height("100dp")
-                                       .withAttribute("android:layout_weight", "1.0")
-                                   ));
-    final SyncNlModel model = builder.build();
-    assertEquals(1, model.getTreeReader().getComponents().size());
-    assertEquals("NlComponent{tag=<LinearLayout>, bounds=[0,0:1000x1000}\n" +
-                 "    NlComponent{tag=<TextView>, bounds=[100,100:100x100}\n" +
-                 "    NlComponent{tag=<Button>, bounds=[100,200:100x100}",
-                 NlTreeDumper.dumpTree(model.getTreeReader().getComponents()));
-    format(model.getFile());
-    return model;
+  @Override
+  public ModelBuilder createModel() {
+    // Layout Structure & Dimension Mappings:
+    //
+    // +------------------------------------------------------------+
+    // | LinearLayout (0, 0, 1000, 1000)                            |
+    // |                                                            |
+    // |  (100, 100)                                                |
+    // |  +-----------------------+                                 |
+    // |  | TextView (myText1)    |                                 |
+    // |  | Initial Size:         |                                 |
+    // |  | width = 100dp         |                                 |
+    // |  | height = 100dp        |                                 |
+    // |  +-----------------------+ (200, 200)                      |
+    // |                                                            |
+    // |  (100, 200)                                                |
+    // |  +-----------------------+                                 |
+    // |  | Button (myText2)      |                                 |
+    // |  | width = 100dp         |                                 |
+    // |  | height = 100dp        |                                 |
+    // |  +-----------------------+ (200, 300)                      |
+    // +------------------------------------------------------------+
+    //
+    return model("linear.xml",
+                 component(LINEAR_LAYOUT)
+                   .withBounds(0, 0, 1000, 1000)
+                   .matchParentWidth()
+                   .matchParentHeight()
+                   .children(
+                     component(TEXT_VIEW)
+                       .withBounds(100, 100, 100, 100)
+                       .id("@id/myText1")
+                       .width("100dp")
+                       .height("100dp"),
+                     component(BUTTON)
+                       .withBounds(100, 200, 100, 100)
+                       .id("@id/myText2")
+                       .width("100dp")
+                       .height("100dp")
+                       .withAttribute("android:layout_weight", "1.0")
+                   ));
   }
 
   /**
