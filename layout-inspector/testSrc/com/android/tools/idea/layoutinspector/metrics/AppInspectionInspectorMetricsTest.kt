c52d55709b400b43bd5a685021274500e3a43886
Pierfrancesco Soffritti <psoffritti@google.com>
2026-07-10 16:43:43 +0200

    Temporarily disable test
    Test: NA
    Bug: 533381063
    Change-Id: I2d7bc12eba592520b746f394727e3c0f63015473

--- layout-inspector/testSrc/com/android/tools/idea/layoutinspector/metrics/AppInspectionInspectorMetricsTest.kt
+++ layout-inspector/testSrc/com/android/tools/idea/layoutinspector/metrics/AppInspectionInspectorMetricsTest.kt
@@ -46,6 +46,7 @@ import com.google.wireless.android.sdk.stats.DynamicLayoutInspectorEvent.Dynamic
 import java.util.concurrent.TimeUnit
 import kotlinx.coroutines.runBlocking
 import org.junit.Before
+import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.RuleChain
@@ -155,6 +156,7 @@ class AppInspectionInspectorMetricsTest {
     }
   }
 
+  @Ignore("b/533381063")
   @Test
   fun testInitialRenderLogging() = runBlocking {
     inspectorRule.launchSynchronously = false
