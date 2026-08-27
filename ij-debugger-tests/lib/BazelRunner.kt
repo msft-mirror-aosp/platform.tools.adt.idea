42f99e45b66221b4e315b5cb386191fa3e492d42
Alon Albert <aalbert@google.com>
2026-08-27 08:15:45 -0700

    Enable k2-tests-jvm in Presubmit
    The test frameworks inserts the entire IDEA log into the Bazel text.xml file for failed tests with results in huge files and exceeds limits. We clean up the test.xml file using the runner wrapper.
    
    Bug: NA
    Test: NA
    Change-Id: Ie0135db6454a04e5b19b78bdc5152094e3b185c5

--- ij-debugger-tests/lib/BazelRunner.kt
+++ ij-debugger-tests/lib/BazelRunner.kt
@@ -17,16 +17,28 @@ package com.google.android.tools.debugger.test.lib
 
 import com.intellij.tests.JUnit5BazelRunner
 import java.io.PrintStream
+import java.nio.file.Path
 import java.util.concurrent.atomic.AtomicBoolean
+import javax.xml.parsers.DocumentBuilderFactory
+import javax.xml.transform.OutputKeys
+import javax.xml.transform.TransformerFactory
+import javax.xml.transform.dom.DOMSource
+import javax.xml.transform.stream.StreamResult
+import javax.xml.xpath.XPathConstants.NODESET
+import javax.xml.xpath.XPathFactory
+import kotlin.io.path.exists
+import org.w3c.dom.NodeList
 
 private const val NO_TESTS_RUN_MSG = "No tests executed: all tests were filtered out by bucketing."
 private val noTestsRun = AtomicBoolean(false)
 private const val NO_TESTS_ERROR = 42 // see org.jetbrains.intellij.build.impl.TestingTasksImpl.NO_TESTS_ERROR
+private val TEST_XML = System.getenv("XML_OUTPUT_FILE")?.let { Path.of(it) }
 
 /**
  * A wrapper of [JUnit5BazelRunner] that modifies its behavior to suit our needs.
  *
  * * If a bad filter is provided, we want the test to fail.
+ * * Remove testcase stderr from test.xml file because the test will dump the entire log there, and it exceeds size limits
  */
 class BazelRunner {
   companion object {
@@ -35,14 +47,15 @@ class BazelRunner {
       // If running in a shard, do not report an error if no tests are executed.
       if (System.getenv("TEST_TOTAL_SHARDS") == null) {
         System.setErr(CheckIfNoTestsRunStream)
-        Runtime.getRuntime().addShutdownHook(ShutdownHook)
       }
+      Runtime.getRuntime().addShutdownHook(ShutdownHook)
       JUnit5BazelRunner.main(args)
     }
   }
 
   private object ShutdownHook : Thread() {
     override fun run() {
+      removeStderrFromTestXml()
       if (noTestsRun.get()) {
         Runtime.getRuntime().halt(NO_TESTS_ERROR)
       }
@@ -71,4 +84,24 @@ private fun checkIfNoTestsRun(str: String?) {
   if (str != null && str.contains(NO_TESTS_RUN_MSG)) {
     noTestsRun.set(true)
   }
+}
+
+private fun removeStderrFromTestXml() {
+  if (TEST_XML == null || !TEST_XML.exists()) {
+    return
+  }
+  try {
+    val doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(TEST_XML.toFile())
+    val xpath = XPathFactory.newInstance().newXPath()
+    val nodes = xpath.evaluate("/testsuites/testsuite/testcase/system-err", doc, NODESET) as NodeList
+    for (i in 0 until nodes.length) {
+      nodes.item(i).textContent = "Elided, use test.log instead "
+    }
+    val transformer =
+      TransformerFactory.newInstance().newTransformer().apply {
+        setOutputProperty(OutputKeys.INDENT, "yes")
+        setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2")
+      }
+    transformer.transform(DOMSource(doc), StreamResult(TEST_XML.toFile()))
+  } catch (_: Exception) {}
 }
